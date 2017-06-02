169ec9ceb1223d28e6425d74c5fba6da2146c7c7
Pfeiffer <pfeiffer@se-rwth.de>
2017-06-02 14:51:28 +0200

    Implemented CoCo that checks: - if only existing ports and variables are initialized in the ajava component initialization - if only existing ports and variables or local variables are used in the compute method of AJava

--- languages/ajava/src/main/java/de/monticore/lang/montiarc/ajava/cocos/AJavaCoCos.java
+++ languages/ajava/src/main/java/de/monticore/lang/montiarc/ajava/cocos/AJavaCoCos.java
@@ -8,7 +8,7 @@ package de.monticore.lang.montiarc.ajava.cocos;
 import de.monticore.lang.montiarc.ajava._cocos.AJavaCoCoChecker;
 import de.monticore.lang.montiarc.ajava.cocos.conventions.AJavaDefinitionUpperCaseCoCo;
 import de.monticore.lang.montiarc.ajava.cocos.conventions.MultipleBehaviorImplementation;
-import de.monticore.lang.montiarc.ajava.cocos.correctness.UsedPortsExistCoCo;
+import de.monticore.lang.montiarc.ajava.cocos.correctness.UsedPortsVariablesExistCoCo;
 import de.monticore.lang.montiarc.cocos.MontiArcCoCos;
 import de.monticore.lang.montiarc.montiarcautomaton.cocos.MontiArcAutomatonCocos;
 
@@ -21,7 +21,7 @@ public class AJavaCoCos {
   public static AJavaCoCoChecker createChecker() {
     AJavaCoCoChecker checker =  new AJavaCoCoChecker();
     checker
-    .addCoCo(new UsedPortsExistCoCo())
+    .addCoCo(new UsedPortsVariablesExistCoCo())
     .addCoCo(new AJavaDefinitionUpperCaseCoCo())
     .addCoCo(new MultipleBehaviorImplementation());
     
