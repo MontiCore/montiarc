b6671bec89493bf0cc61cf5992af067e2ff85b70
Pfeiffer <pfeiffer@se-rwth.de>
2017-11-06 13:30:30 +0100

    refactored UniquenessTest

--- languages/montiarc-fe/src/test/java/contextconditions/UniquenessTest.java
+++ languages/montiarc-fe/src/test/java/contextconditions/UniquenessTest.java
@@ -5,8 +5,6 @@
  */
 package contextconditions;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 
 import org.antlr.v4.runtime.RecognitionException;
@@ -14,6 +12,9 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import de.se_rwth.commons.logging.Log;
+import montiarc._cocos.MontiArcCoCoChecker;
+import montiarc.cocos.PortNamesAreUnique;
+import montiarc.cocos.UniqueConstraint;
 
 /**
  * Tests various forms of component uniqueness
@@ -21,33 +22,31 @@ import de.se_rwth.commons.logging.Log;
  * @author Robert Heim
  */
 public class UniquenessTest extends AbstractCoCoTest {
-
+  
   @BeforeClass
   public static void setUp() {
     Log.getFindings().clear();
     Log.enableFailQuick(false);
   }
-
+  
   @Test
   public void testPortNamingUnique() throws RecognitionException, IOException {
-    runCheckerWithSymTab("arc/coco/uniqueness", "a.E1");
-    assertEquals(2, Log.getFindings().stream().filter(f -> f.buildMsg().contains("xMA053"))
-        .count());
+    checkInvalid(new MontiArcCoCoChecker().addCoCo(new PortNamesAreUnique()),
+        getAstNode("arc/coco/uniqueness", "a.E1"), new ExpectedErrorInfo(2, "xMA053"));
   }
-
+  
   @Test
   public void testPortImplicitNamingOneExplicitNamed()
       throws RecognitionException, IOException {
-    runCheckerWithSymTab("arc/coco/uniqueness", "a.E1_2");
-    assertEquals(1,
-        Log.getFindings().stream().filter(f -> f.buildMsg().contains("xMA053")).count());
+    checkInvalid(new MontiArcCoCoChecker().addCoCo(new PortNamesAreUnique()),
+        getAstNode("arc/coco/uniqueness", "a.E1_2"), new ExpectedErrorInfo(1, "xMA053"));
   }
-
+  
   @Test
   public void testUniqueConstraints() throws RecognitionException, IOException {
-    runCheckerWithSymTab("arc/coco/uniqueness", "a.UniqueConstraint");
-    assertEquals(2, Log.getFindings().stream().filter(f -> f.buildMsg().contains("xMA052"))
-        .count());
+    checkInvalid(new MontiArcCoCoChecker().addCoCo(new UniqueConstraint()),
+        getAstNode("arc/coco/uniqueness", "a.UniqueConstraint"),
+        new ExpectedErrorInfo(2, "xMA052"));
   }
-
+  
 }
