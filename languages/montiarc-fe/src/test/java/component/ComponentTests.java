56e564a3041f2801fd0e5c310d7e12f29174b8df
wortmann <wortmann@se-rwth.de>
2018-01-30 14:21:07 +0100

    Moved tests

--- languages/montiarc-fe/src/test/java/component/ComponentTests.java
+++ languages/montiarc-fe/src/test/java/component/ComponentTests.java
@@ -1,12 +1,20 @@
 package component;
 
+import de.monticore.java.symboltable.JavaFieldSymbol;
+import de.monticore.java.symboltable.JavaTypeSymbol;
+import de.monticore.symboltable.Scope;
+import de.monticore.umlcd4a.symboltable.CDFieldSymbol;
+import de.monticore.umlcd4a.symboltable.CDTypeSymbol;
 import de.se_rwth.commons.logging.Log;
+import montiarc.MontiArcTool;
 import montiarc._ast.ASTComponent;
 import montiarc._ast.ASTMontiArcNode;
 import montiarc._cocos.MontiArcCoCoChecker;
 import montiarc.cocos.ConnectorSourceAndTargetComponentDiffer;
 import montiarc.cocos.ImportsAreUnique;
 import montiarc.cocos.MontiArcCoCos;
+
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -27,9 +35,12 @@ public class ComponentTests extends AbstractCoCoTest {
   
   private static final String PACKAGE = "component";
   
+private static MontiArcTool tool;
+  
   @BeforeClass
   public static void setUp() {
     Log.enableFailQuick(false);
+    tool = new MontiArcTool();
   }
   
   @Test
@@ -39,6 +50,31 @@ public class ComponentTests extends AbstractCoCoTest {
     ASTMontiArcNode node = getAstNode(MP, PACKAGE + "." + "RedundantImports");
     MontiArcCoCoChecker cocos = new MontiArcCoCoChecker().addCoCo(new ImportsAreUnique());
     checkInvalid(cocos, node, new AbstractCoCoTestExpectedErrorInfo(2, "xMA074"));
+  }
+  
+  @Test
+  public void testCDType2JavaType() {
+    Scope symTab = tool.createSymbolTable("src/test/resources/");
+    CDTypeSymbol cdType = symTab
+        .<CDTypeSymbol> resolve("types.Datatypes.MotorCommand", CDTypeSymbol.KIND)
+        .orElse(null);
+    assertNotNull(cdType);
+    JavaTypeSymbol javaType = symTab
+        .<JavaTypeSymbol> resolve("types.Datatypes.MotorCommand", JavaTypeSymbol.KIND)
+        .orElse(null);
+    assertNotNull(javaType);
+  }
+  
+  @Test
+  public void testCDField2JavaField() {
+    Scope symTab = tool.createSymbolTable("src/test/resources/");
+    CDFieldSymbol cdField = symTab
+        .<CDFieldSymbol> resolve("types.Datatypes.MotorCommand.STOP", CDFieldSymbol.KIND)
+        .orElse(null);
+    assertNotNull(cdField);
+    JavaFieldSymbol javaField = symTab.<JavaFieldSymbol> resolve(
+        "types.Datatypes.MotorCommand.STOP", JavaFieldSymbol.KIND).orElse(null);
+    assertNotNull(javaField);
   }
   
 }
