4252f3c2a5e282b52c6aa1cc6c85f280710917a7
wortmann <wortmann@se-rwth.de>
2018-02-07 15:43:36 +0100

    Wie kommt man in CoCos wohl an das verursachende Modell

--- languages/montiarc-fe/src/main/java/montiarc/cocos/UseOfUndeclaredField.java
+++ languages/montiarc-fe/src/main/java/montiarc/cocos/UseOfUndeclaredField.java
@@ -1,24 +1,21 @@
 package montiarc.cocos;
 
+import de.monticore.symboltable.Scope;
 import de.se_rwth.commons.logging.Log;
+import montiarc._ast.ASTIOAssignment;
+import montiarc._ast.ASTPort;
+import montiarc._ast.ASTVariableDeclaration;
 import montiarc._cocos.MontiArcASTIOAssignmentCoCo;
 import montiarc._symboltable.PortSymbol;
 import montiarc._symboltable.VariableSymbol;
-import montiarc._ast.ASTIOAssignment;
-
-import java.util.Optional;
-import montiarc._ast.ASTPort;
-import montiarc._ast.ASTVariableDeclaration;
-import de.monticore.symboltable.Scope;
 
 /**
- * Context condition for checking, if a reference is used inside an automaton
- * which has not been defined in an {@link ASTVariableDeclaration} or as {@link ASTPort}.
+ * Context condition for checking, if a reference is used inside an automaton which has not been
+ * defined in an {@link ASTVariableDeclaration} or as {@link ASTPort}.
  *
- * @implements [Wor16] AR1: Names used in guards, valuations, and assignments exist in the automaton. (p. 102, Lst. 5.19)
- *
- * @author Gerrit Leonhardt
- *
+ * @implements [Wor16] AR1: Names used in guards, valuations, and assignments exist in the
+ * automaton. (p. 102, Lst. 5.19)
+ * @author Gerrit Leonhardt, Andreas Wortmann
  */
 public class UseOfUndeclaredField implements MontiArcASTIOAssignmentCoCo {
   
@@ -26,20 +23,17 @@ public class UseOfUndeclaredField implements MontiArcASTIOAssignmentCoCo {
   public void check(ASTIOAssignment node) {
     // only check left side of IOAssignment, right side is implicitly checked
     // when resolving type of the valuations
-    if (node.nameIsPresent()) {
-    	
-      Scope scope = node.getEnclosingScope().get();
-      boolean foundVar = scope.resolve(node.getName().get(), VariableSymbol.KIND).isPresent();
-      boolean foundPort = scope.resolve(node.getName().get(), PortSymbol.KIND).isPresent();
-      String refKind = foundVar ? "Variable" : "Port";
-      if (!foundVar) {
-    	  if(!foundPort) {
-    		  Log.error("0xMA079: "+refKind+" " + node.getName().get() + " is used as a field, but is not declared as such.", node.get_SourcePositionStart());
-    	      
-    	  }
-        }
     
-      
+    if (node.nameIsPresent()) {
+      String name = node.getName().get();
+      Scope scope = node.getEnclosingScope().get();
+      boolean foundVar = scope.resolve(name, VariableSymbol.KIND).isPresent();
+      boolean foundPort = scope.resolve(name, PortSymbol.KIND).isPresent();
+      if (!foundVar && !foundPort) {
+        Log.error("0xMA079: The name '" + name + "' is used in assignment, but is neither declared "
+            + "a port, nor as a variable.",
+            node.get_SourcePositionStart());
+      }
     }
   }
   
