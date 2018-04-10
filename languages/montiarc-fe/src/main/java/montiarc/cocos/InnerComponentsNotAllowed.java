79b7474c18ac92f5351f18f2e1e841cd949d050c
Pfeiffer <pfeiffer@se-rwth.de>
2018-04-10 13:24:09 +0200

    Coco for forbidding inner component definitions

---
+++ languages/montiarc-fe/src/main/java/montiarc/cocos/InnerComponentsNotAllowed.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018 RWTH Aachen. All rights reserved.
+ *
+ * http://www.se-rwth.de/
+ */
+package montiarc.cocos;
+
+import de.se_rwth.commons.logging.Log;
+import montiarc._ast.ASTComponent;
+import montiarc._cocos.MontiArcASTComponentCoCo;
+import montiarc._cocos.MontiArcASTElementCoCo;
+import montiarc._symboltable.ComponentSymbol;
+
+/**
+ * This coco forbids the definition of inner components
+ *
+ * @author Jerome Pfeiffer
+ * @version $Revision$, $Date$
+ */
+public class InnerComponentsNotAllowed implements MontiArcASTComponentCoCo {
+
+  /**
+   * @see montiarc._cocos.MontiArcASTComponentCoCo#check(montiarc._ast.ASTComponent)
+   */
+  @Override
+  public void check(ASTComponent node) {
+    ComponentSymbol sym = (ComponentSymbol) node.getSymbol().get();
+    if(sym.isInnerComponent()) {
+      Log.error("0xMA095 Inner component definitions are forbidden.",node.get_SourcePositionStart());
+    }
+  }
+  
+}
