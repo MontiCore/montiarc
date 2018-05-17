c072d2c6ab83cf6a624d059c79ce157ae39a456d
Pfeiffer <pfeiffer@se-rwth.de>
2018-05-17 17:52:57 +0200

    #102 added forbidden java constructs to cocos/UseOfForbiddenExpression.java

--- languages/montiarc-fe/src/main/java/montiarc/cocos/UseOfForbiddenExpression.java
+++ languages/montiarc-fe/src/main/java/montiarc/cocos/UseOfForbiddenExpression.java
@@ -5,20 +5,35 @@ import de.monticore.java.javadsl._cocos.JavaDSLASTExpressionCoCo;
 import de.se_rwth.commons.logging.Log;
 
 /**
- * Context condition for checking, if all expressions used are allowed expressions.
+ * Context condition for checking, if all expressions used are allowed
+ * expressions.
  * 
- * @implements [Wor16] AC5: The automaton’s valuations and assignments use only allowed Java/P
- * modeling elements.
+ * @implements [Wor16] AC5: The automaton’s valuations and assignments use only
+ * allowed Java/P modeling elements.
  * @author Gerrit Leonhardt
  */
 public class UseOfForbiddenExpression implements JavaDSLASTExpressionCoCo {
   
   @Override
   public void check(ASTExpression node) {
+    boolean isError = false;
+    String errorMessage = "";
     if (node.instanceofTypeIsPresent()) {
-      Log.error("0xMA023 Expression contains forbidden expression: instanceOf expression",
+      errorMessage = "instanceOf expression";
+    }
+    else if (node.arrayExpressionIsPresent()) {
+      errorMessage = "array access expression";
+    }
+    else if(node.typeCastTypeIsPresent()) {
+      errorMessage = "type cast expression";
+    }
+    else if(node.prefixOpIsPresent()) {
+      errorMessage = "prefix expression";
+    }
+    
+    if (isError) {
+      Log.error("0xMA023 Expression contains forbidden expression: " + errorMessage,
           node.get_SourcePositionStart());
     }
-    // TODO: Auch Schleifen und anderen Kram verbieten. Siehe [Wor16].
   }
 }
