954662fe7227e1f13e5789199eedc735eb0cb112
Butting <butting@se-rwth.de>
2019-04-03 10:57:20 +0200

    Fixed bug in Port2FieldResolvingFIlter (#260) and a test to check that this bug never occurs again

--- languages/montiarc-fe/src/main/java/montiarc/_symboltable/adapters/Port2FieldResolvingFilter.java
+++ languages/montiarc-fe/src/main/java/montiarc/_symboltable/adapters/Port2FieldResolvingFilter.java
@@ -1,7 +1,15 @@
 package montiarc._symboltable.adapters;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import com.google.common.collect.Lists;
+
 import de.monticore.java.symboltable.JavaFieldSymbol;
 import de.monticore.symboltable.Symbol;
+import de.monticore.symboltable.resolving.ResolvingInfo;
 import de.monticore.symboltable.resolving.TransitiveAdaptedResolvingFilter;
 import montiarc._symboltable.PortSymbol;
 
@@ -14,6 +22,32 @@ public class Port2FieldResolvingFilter extends TransitiveAdaptedResolvingFilter<
   @Override
   public Symbol translate(Symbol s) {
     return new Port2FieldAdapter((PortSymbol) s);
+  }
+  
+  @Override
+  public Collection<Symbol> filter(ResolvingInfo resolvingInfo, Collection<Symbol> symbols) {
+	  Collection<Symbol> result = Lists.newArrayList();
+	  for(Symbol s : symbols) {
+		  Optional<Symbol> foundSymbol = super.filter(resolvingInfo, s.getName(), getSymbolMap(symbols));
+		  if(foundSymbol.isPresent()) {
+			  result.add(foundSymbol.get());
+		  }
+	  }
+	  return result;
+  }
+  
+  protected Map<String,Collection<Symbol>> getSymbolMap(Collection<Symbol> symbols){
+	  Map<String,Collection<Symbol>> symbolMap = new HashMap<>();
+	  for(Symbol s: symbols) {
+		  String name = s.getName();
+		  if(!symbolMap.containsKey(name)) {
+			  symbolMap.put(name, Lists.newArrayList(s));
+		  }
+		  else {
+			  symbolMap.get(name).add(s);
+		  }
+	  }
+	  return symbolMap;
   }
   
 }
