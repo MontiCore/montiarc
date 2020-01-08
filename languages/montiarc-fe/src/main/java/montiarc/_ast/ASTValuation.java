2e3b82ad61de170467ff98f059bef3465d634eb6
schmalzing <schmalzing@se-rwth.de>
2020-01-08 18:06:16 +0100

    Remove unused AST classes

--- languages/montiarc-fe/src/main/java/montiarc/_ast/ASTValuation.java
+++
@@ -1,8 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package montiarc._ast;
-
-import de.monticore.mcexpressions._ast.ASTExpression;
-
-public interface ASTValuation extends ASTValuationTOP {
-  public ASTExpression getExpression();
-}
