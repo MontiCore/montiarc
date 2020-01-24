38d8cfe0b6f207f65b4e3524eccdbfd0469938a6
Manuel Pützer <puetzer@se-rwth.de>
2020-01-24 15:10:39 +0100

    Replaced System.out with a Logger
    Log-Level for generated components can be configured

--- libraries/maa-rte/src/main/java/de/montiarcautomaton/runtimes/Log.java
+++ libraries/maa-rte/src/main/java/de/montiarcautomaton/runtimes/Log.java
@@ -1,26 +1,32 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.montiarcautomaton.runtimes;
 
+import org.slf4j.LoggerFactory;
+
 /**
  * Default methods for logging.
  * 
  * @author Gerrit Leonhardt
  */
 public class Log {
+
   public static void warn(String tag, String msg) {
-    log(tag, msg);
+    LoggerFactory.getLogger(tag).warn(msg);
   }
   
   public static void error(String tag, String msg) {
-    log(tag, msg);
+    LoggerFactory.getLogger(tag).error(msg);
   }
   
   public static void error(String tag, Exception e) {
-    System.out.println("[" + tag + "]:");
-    e.printStackTrace();
+    LoggerFactory.getLogger(tag).error(e.getMessage(),e);
+  }
+
+  public static void error(String tag, String msg, Exception e){
+    LoggerFactory.getLogger(tag).error(msg,e);
   }
   
   public static void log(String tag, String msg) {
-    System.out.println("[" + tag + "]:\n" + msg);
+    LoggerFactory.getLogger(tag).info(msg);
   }
 }
