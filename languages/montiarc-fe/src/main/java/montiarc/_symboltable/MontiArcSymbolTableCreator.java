90bc402c955b1cb31244d4b7aa5b728608be7dd6
schmalzing <schmalzing@se-rwth.de>
2020-07-08 00:22:31 +0200

    Update symbol table creator with handling of package and import statements

---
+++ languages/montiarc-fe/src/main/java/montiarc/_symboltable/MontiArcSymbolTableCreator.java
@@ -0,0 +1,40 @@
+/* (c) https://github.com/MontiCore/monticore */
+package montiarc._symboltable;
+
+import com.google.common.base.Preconditions;
+import de.monticore.symboltable.ImportStatement;
+import de.monticore.types.mcbasictypes._ast.ASTMCImportStatement;
+import montiarc._ast.ASTMACompilationUnit;
+import org.codehaus.commons.nullanalysis.NotNull;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class MontiArcSymbolTableCreator extends MontiArcSymbolTableCreatorTOP {
+
+  public MontiArcSymbolTableCreator(@NotNull IMontiArcScope enclosingScope) {
+    super(enclosingScope);
+  }
+
+  public MontiArcSymbolTableCreator(
+    @NotNull Deque<? extends IMontiArcScope> scopeStack) {
+    super(scopeStack);
+  }
+
+  @Override
+  public MontiArcArtifactScope createFromAST(@NotNull ASTMACompilationUnit rootNode) {
+    Preconditions.checkArgument(rootNode != null);
+    List<ImportStatement> imports = new ArrayList<>();
+    for (ASTMCImportStatement importStatement : rootNode.getImportStatementList()) {
+      imports.add(new ImportStatement(importStatement.getQName(), importStatement.isStar()));
+    }
+    MontiArcArtifactScope artifactScope = montiarc.MontiArcMill.montiArcArtifactScopeBuilder()
+      .setPackageName(rootNode.getPackage().getQName())
+      .setImportList(imports)
+      .build();
+    putOnStack(artifactScope);
+    rootNode.accept(getRealThis());
+    return artifactScope;
+  }
+}
