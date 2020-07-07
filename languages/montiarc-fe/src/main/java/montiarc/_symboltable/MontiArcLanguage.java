8be79dca5b989cae47cd4d756606e9fd3bba2f32
christian.kirchhof <christian.kirchhof@rwth-aachen.de>
2020-07-07 15:57:03 +0200

    Fix NullPointerException
    For some reason modelloader is null if initialized by
    assigning it to the instance variable.

--- languages/montiarc-fe/src/main/java/montiarc/_symboltable/MontiArcLanguage.java
+++ languages/montiarc-fe/src/main/java/montiarc/_symboltable/MontiArcLanguage.java
@@ -4,21 +4,23 @@ package montiarc._symboltable;
 
 public class MontiArcLanguage extends MontiArcLanguageTOP {
 
-  protected MontiArcModelLoader modelLoader = new MontiArcModelLoader(this);
+  protected MontiArcModelLoader modelLoader;
 
   public MontiArcLanguage(String langName, String fileEnding) {
     super(langName, fileEnding);
+    this.modelLoader = super.getModelLoader();
   }
 
   public static final String FILE_ENDING = "mc4";
 
   public MontiArcLanguage() {
     super("MontiArc", FILE_ENDING);
+    this.modelLoader = super.getModelLoader();
   }
 
   @Override
   protected MontiArcModelLoader provideModelLoader() {
-    return this.modelLoader;
+    return new MontiArcModelLoader(this);
   }
 
   @Override
