e617a876b41f343f62d5f474f42ee93b6e9aeaba
Julian Jérôme Krebber <julian.krebber@rwth-aachen.de>
2020-09-09 21:47:58 +0200

    Added fully-connected and port-complete property.

--- languages/arc-fe/src/main/java/comfortablearc/_symboltable/ComfortableArcSymbolTableCreator.java
+++ languages/arc-fe/src/main/java/comfortablearc/_symboltable/ComfortableArcSymbolTableCreator.java
@@ -1,6 +1,8 @@
 /* (c) https://github.com/MontiCore/monticore */
 package comfortablearc._symboltable;
 
+import arcbasis._ast.ASTComponentInstantiation;
+import comfortablearc._ast.ASTFullyConnectedComponentInstantiation;
 import org.codehaus.commons.nullanalysis.NotNull;
 
 import java.util.Deque;
@@ -14,5 +16,15 @@ public class ComfortableArcSymbolTableCreator extends ComfortableArcSymbolTableC
   public ComfortableArcSymbolTableCreator(
     @NotNull Deque<? extends IComfortableArcScope> scopeStack) {
     super(scopeStack);
+  }
+
+  @Override
+  public void visit(@NotNull ASTFullyConnectedComponentInstantiation node) {
+    getRealThis().visit((ASTComponentInstantiation) node);
+  }
+
+  @Override
+  public void endVisit(@NotNull ASTFullyConnectedComponentInstantiation node) {
+    getRealThis().endVisit((ASTComponentInstantiation) node);
   }
 }
