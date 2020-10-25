5dfd5518602a8cf6238ec376d555101e3fef0516
David Schmalzing <schmalzing@se-rwth.de>
2020-10-25 14:44:46 +0100

    Move abstract test of utils and add abstract test for montiarc

--- util/src/test/java/montiarc/util/ErrorCodeTest.java
+++ util/src/test/java/montiarc/util/ErrorCodeTest.java
@@ -1,7 +1,6 @@
 /* (c) https://github.com/MontiCore/monticore */
 package montiarc.util;
 
-import montiarc.AbstractTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
