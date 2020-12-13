24ba284c7b9f0ce4f4e0aa6d3bf471d812626ea0
David Schmalzing <schmalzing@se-rwth.de>
2020-12-13 18:36:15 +0100

    Revert CD version to 1.7.0

--- generators/cd2pojo/src/main/java/de/monticore/cd2pojo/TypeHelper.java
+++ generators/cd2pojo/src/main/java/de/monticore/cd2pojo/TypeHelper.java
@@ -1,16 +1,16 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.cd2pojo;
 
-import de.monticore.cdbasis._symboltable.CDTypeSymbol;
+import de.monticore.cd.cd4analysis._symboltable.CDTypeSymbol;
 
 public class TypeHelper {
-  
+
   private String _package;
-  
+
   public TypeHelper(String _package) {
     this._package = _package;
   }
-  
+
   public String printType(CDTypeSymbol type) {
     return _package + "." + type.getName();
   }
