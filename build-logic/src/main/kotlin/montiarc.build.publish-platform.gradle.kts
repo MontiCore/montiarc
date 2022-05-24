d918692d79f69c490a251cd6e3c81a5b9e58eb2c
David Schmalzing <schmalzing@se-rwth.de>
2022-05-24 14:39:39 +0200

    Extract common code

--- build-logic/src/main/kotlin/montiarc.build.publish-platform.gradle.kts
+++ build-logic/src/main/kotlin/montiarc.build.publish-platform.gradle.kts
@@ -1,32 +1,17 @@
 /* (c) https://github.com/MontiCore/monticore */
+import montiarc.build.Publish.Companion.configurePublication
 
 plugins {
   id("montiarc.build.publish-base")
 }
 
 configure<PublishingExtension> {
-  publications.create<MavenPublication>("mavenPlatform") {
+  publications.create<MavenPublication>("platform") {
     configurePlatformPublication()
   }
 }
 
 fun MavenPublication.configurePlatformPublication() {
   from(components["javaPlatform"])
-  artifactId = project.name
-  pom {
-    url.set("https://github.com/MontiCore/montiarc")
-    licenses {
-      license {
-        name.set("MontiCore-3-Level-License-Model")
-        url.set("https://monticore.github.io/monticore/00.org/Licenses/LICENSE-MONTICORE-3-LEVEL/")
-      }
-    }
-    developers {
-      developer {
-        name.set("The MontiArc Team")
-        organization.set("Software Engineering RWTH Aachen University")
-        organizationUrl.set("https://monticore.de/")
-      }
-    }
-  }
+  configurePublication(project)
 }
---
+++ build-logic/src/main/kotlin/montiarc/build/Publish.kt
@@ -0,0 +1,27 @@
+/* (c) https://github.com/MontiCore/monticore */
+package montiarc.build
+
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.*
+
+class Publish {
+  companion object {
+    fun MavenPublication.configurePublication(project: Project) {
+      artifactId = project.name
+      pom.url.set("https://github.com/MontiCore/montiarc")
+      pom.licenses {
+        this.license {
+          this.name.set("MontiCore-3-Level-License-Model")
+          this.url.set("https://monticore.github.io/monticore/00.org/Licenses/LICENSE-MONTICORE-3-LEVEL/")
+        }
+      }
+      pom.developers {
+        this.developer {
+          this.name.set("The MontiArc Team")
+          this.organization.set("Software Engineering RWTH Aachen University")
+          this.organizationUrl.set("https://monticore.de/")
+        }
+      }
+    }
+  }
+}
