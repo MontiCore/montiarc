49a09aa7db9f84e5ef4c51c55d93c40355492e87
David Schmalzing <schmalzing@se-rwth.de>
2022-08-29 11:47:14 +0200

    Remove unused trafo classes

--- languages/montiarc/main/java/montiarc/trafos/MACompilationUnitTransformer.java
+++
@@ -1,9 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package montiarc.trafos;
-
-import montiarc._ast.ASTMACompilationUnit;
-
-public interface MACompilationUnitTransformer {
-
-  void transform(ASTMACompilationUnit node);
-}
--- languages/montiarc/main/java/montiarc/trafos/MontiArcNodeTransformer.java
+++
@@ -1,9 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package montiarc.trafos;
-
-import montiarc._ast.ASTMontiArcNode;
-
-public interface MontiArcNodeTransformer {
-
-  void transform(ASTMontiArcNode node);
-}
