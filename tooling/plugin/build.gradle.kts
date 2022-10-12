2552889d919242c38e4c667ebf0ee5da0dfabb5a
David Pascal Schmalzing <schmalzing@se-rwth.de>
2022-10-12 12:39:35 +0200

    Fix plugin publish

--- tooling/plugin/build.gradle.kts
+++ tooling/plugin/build.gradle.kts
@@ -1,9 +1,12 @@
 /* (c) https://github.com/MontiCore/monticore */
 
 plugins {
+  kotlin("jvm") version "1.5.31"
   id("java-gradle-plugin")
   id("montiarc.build.project-version")
-  id("montiarc.build.kotlin-library")
+  id("montiarc.build.modules")
+  id("montiarc.build.repositories")
+  id("montiarc.build.publish-base")
 }
 
 group = "montiarc.tooling"
@@ -13,6 +16,19 @@ gradlePlugin {
     create("Montiarc") {
       id = "montiarc"
       implementationClass = "montiarc.tooling.plugin.MontiarcPlugin"
+    }
+  }
+}
+
+kotlin {
+  sourceSets {
+    main {
+      kotlin.setSrcDirs(setOf("main/kotlin"))
+      resources.setSrcDirs(setOf("main/resources"))
+    }
+    test {
+      kotlin.setSrcDirs(setOf("test/kotlin"))
+      resources.setSrcDirs(setOf("test/resources"))
     }
   }
 }
