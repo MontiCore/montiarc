95ea28adc141b56ed3cea0a2888f25aa10fe0f25
David Schmalzing <schmalzing@se-rwth.de>
2022-11-21 16:08:32 +0100

    MontiArc 7.4.0

--- build-logic/src/main/kotlin/montiarc.build.project-version.gradle.kts
+++ build-logic/src/main/kotlin/montiarc.build.project-version.gradle.kts
@@ -1,7 +1,7 @@
 /* (c) https://github.com/MontiCore/monticore */
 
 // We declare the project version here in order to be able to sync it among all composite build parts.
-val commonVersion = "7.4.0-SNAPSHOT"
+val commonVersion = "7.4.0"
 
 version = commonVersion
 
