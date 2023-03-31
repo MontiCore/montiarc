a8fd668b4ab3e09135ea2a62dd1b51db613d47d1
David Schmalzing <schmalzing@se-rwth.de>
2023-03-31 20:25:24 +0200

    Rework ports and subports connected tests

--- languages/montiarc/test/java/montiarc/_cocos/PortsConnectedTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/PortsConnectedTest.java
@@ -3,79 +3,202 @@ package montiarc._cocos;
 
 import arcbasis._cocos.PortsConnected;
 import com.google.common.base.Preconditions;
+import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
 import de.se_rwth.commons.logging.Log;
+import de.se_rwth.commons.logging.LogStub;
+import montiarc.MontiArcAbstractTest;
+import montiarc.MontiArcMill;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.ArcError;
 import montiarc.util.Error;
+import org.assertj.core.api.Assertions;
 import org.codehaus.commons.nullanalysis.NotNull;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
 import java.util.stream.Stream;
 
-public class PortsConnectedTest extends AbstractCoCoTest {
+/**
+ * The class under test is {@link PortsConnected}.
+ */
+public class PortsConnectedTest extends MontiArcAbstractTest {
 
-    protected static String PACKAGE = "portsConnected";
+  @BeforeAll
+  public static void init() {
+    LogStub.init();
+    Log.enableFailQuick(false);
+    MontiArcMill.reset();
+    MontiArcMill.init();
+    BasicSymbolsMill.initializePrimitives();
+    setUpComponents();
+  }
 
-    @Override
-    @BeforeEach
-    public void setUp() {
-        super.setUp();
-    }
+  @Override
+  public void setUp() {
+  }
 
-    @Override
-    protected String getPackage() {
-        return PACKAGE;
-    }
+  @AfterEach
+  public void tearDown() {
+    Log.clearFindings();
+  }
 
-    @Override
-    protected void registerCoCos(@NotNull MontiArcCoCoChecker checker) {
-        Preconditions.checkNotNull(checker);
-        checker.addCoCo(new PortsConnected());
-    }
+  protected static void setUpComponents() {
+    compile("package a.b; component A { }");
+    compile("package a.b; component B { port in int i; }");
+    compile("package a.b; component C { port out int o; }");
+    compile("package a.b; component D { port in int i; port out int o; }");
+    compile("package a.b; component E { port in int i1, i2; }");
+    compile("package a.b; component F { port out int o1, o2; }");
+  }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"AllPortsConnected.arc"})
-    public void shouldApproveValidPortConnections(@NotNull String model) {
-        Preconditions.checkNotNull(model);
+  @ParameterizedTest
+  @ValueSource(strings = {
+    "component Comp1 { }",
+    "component Comp2 { " +
+      "a.b.A a; " +
+      "}",
+    "component Comp3 { " +
+      "port in int i; " +
+      "a.b.B b; " +
+      "i -> b.i; " +
+      "}",
+    "component Comp4 { " +
+      "port out int o; " +
+      "a.b.C c; " +
+      "c.o -> o; " +
+      "}",
+    "component Comp5 { " +
+      "port out int o1, o2; " +
+      "a.b.C c; " +
+      "c.o -> o1, o2; " +
+      "}",
+    "component Comp6 { " +
+      "port in int i; " +
+      "port out int o; " +
+      "a.b.D d; " +
+      "i -> d.i; " +
+      "d.o -> o; " +
+      "}",
+    "component Comp7 { " +
+      "port in int i; " +
+      "a.b.E e; " +
+      "i -> e.i1; " +
+      "i -> e.i2; " +
+      "}",
+    "component Comp8 { " +
+      "port in int i; " +
+      "a.b.E e; " +
+      "i -> e.i1, e.i2; " +
+      "}",
+    "component Comp9 { " +
+      "port out int o1, o2; " +
+      "a.b.F f; " +
+      "f.o1 -> o1; " +
+      "f.o2 -> o2; " +
+      "}",
+    "component Comp10 { " +
+      "port in int i; " +
+      "port out int o; " +
+      "component Inner {" +
+      "port in int i; " +
+      "port out int o; " +
+      "}" +
+      "Inner sub; " +
+      "i -> sub.i; " +
+      "sub.o -> o; " +
+      "}"
+  })
+  public void shouldNotReportError(@NotNull String model) throws IOException {
+    Preconditions.checkNotNull(model);
 
-        //Given
-        ASTMACompilationUnit ast = this.parseAndCreateAndCompleteSymbols(model);
+    // Given
+    ASTMACompilationUnit ast = MontiArcMill.parser().parse_StringMACompilationUnit(model).orElseThrow();
+    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
+    MontiArcMill.symbolTableCompleterDelegator().createFromAST(ast);
 
-        //When
-        this.getChecker().checkAll(ast);
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
+    checker.addCoCo(new PortsConnected());
 
-        //Then
-        Assertions.assertEquals(0, Log.getFindingsCount());
-    }
+    // When
+    checker.checkAll(ast);
 
-    protected static Stream<Arguments> modelAndExpectedErrorsProvider() {
-        return Stream.of(
-                arg("IncomingPortNotConnected.arc", ArcError.IN_PORT_UNUSED),
-                arg("OutgoingPortNotConnected.arc", ArcError.OUT_PORT_UNUSED),
-                arg("MissingPortConnections.arc",
-                        ArcError.IN_PORT_UNUSED, ArcError.OUT_PORT_UNUSED)
-        );
-    }
+    // Then
+    Assertions.assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+  }
 
-    @ParameterizedTest
-    @MethodSource("modelAndExpectedErrorsProvider")
-    public void shouldFindInvalidTypes(@NotNull String model, @NotNull Error... errors) {
-        Preconditions.checkNotNull(model);
-        Preconditions.checkNotNull(errors);
+  @ParameterizedTest
+  @MethodSource("invalidModels")
+  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+    Preconditions.checkNotNull(model);
+    Preconditions.checkNotNull(errors);
 
-        //Given
-        ASTMACompilationUnit ast = this.parseAndCreateSymbols(model);
+    // Given
+    ASTMACompilationUnit ast = MontiArcMill.parser().parse_StringMACompilationUnit(model).orElseThrow();
+    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
+    MontiArcMill.symbolTableCompleterDelegator().createFromAST(ast);
 
-        //When
-        this.getChecker().checkAll(ast);
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
+    checker.addCoCo(new PortsConnected());
 
-        //Then
-        this.checkOnlyExpectedErrorsPresent(errors);
-    }
+    // When
+    checker.checkAll(ast);
+
+    // Then
+    Assertions.assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
+    Assertions.assertThat(this.collectErrorCodes(Log.getFindings())).as(Log.getFindings().toString())
+      .containsExactlyElementsOf(this.collectErrorCodes(errors));
+  }
+
+  protected static Stream<Arguments> invalidModels() {
+    return Stream.of(
+      arg("component Comp1 { " +
+          "port in int i; " +
+          "a.b.A a; " +
+          "}",
+        ArcError.IN_PORT_UNUSED),
+      arg("component Comp2 { " +
+          "port out int o; " +
+          "a.b.A a; " +
+          "}",
+        ArcError.OUT_PORT_UNUSED),
+      arg("component Comp3 { " +
+          "port in int i; " +
+          "port out int o; " +
+          "a.b.A a; " +
+          "}",
+        ArcError.IN_PORT_UNUSED,
+        ArcError.OUT_PORT_UNUSED),
+      arg("component Comp4 { " +
+          "port in int i1, i2; " +
+          "a.b.A a; " +
+          "}",
+        ArcError.IN_PORT_UNUSED,
+        ArcError.IN_PORT_UNUSED),
+      arg("component Comp5 { " +
+          "port out int o1, o2; " +
+          "a.b.A a; " +
+          "}",
+        ArcError.OUT_PORT_UNUSED,
+        ArcError.OUT_PORT_UNUSED),
+      arg("component Comp6 { " +
+          "port in int i; " +
+          "port out int o; " +
+          "component Inner { " +
+          "port in int i; " +
+          "port out int o; " +
+          "a.b.A a; " +
+          "} " +
+          "Inner sub; " +
+          "}",
+        ArcError.IN_PORT_UNUSED,
+        ArcError.IN_PORT_UNUSED,
+        ArcError.OUT_PORT_UNUSED,
+        ArcError.OUT_PORT_UNUSED)
+    );
+  }
 }
-
--- languages/montiarc/test/java/montiarc/_cocos/SubPortsConnectedTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/SubPortsConnectedTest.java
@@ -3,82 +3,198 @@ package montiarc._cocos;
 
 import arcbasis._cocos.SubPortsConnected;
 import com.google.common.base.Preconditions;
+import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
 import de.se_rwth.commons.logging.Log;
+import de.se_rwth.commons.logging.LogStub;
+import montiarc.MontiArcAbstractTest;
+import montiarc.MontiArcMill;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.ArcError;
 import montiarc.util.Error;
+import org.assertj.core.api.Assertions;
 import org.codehaus.commons.nullanalysis.NotNull;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
 import java.util.stream.Stream;
 
-public class SubPortsConnectedTest extends AbstractCoCoTest {
+/**
+ * The class under test is {@link SubPortsConnected}.
+ */
+public class SubPortsConnectedTest extends MontiArcAbstractTest {
 
-  protected static String PACKAGE = "subPortsConnected";
+  @BeforeAll
+  public static void init() {
+    LogStub.init();
+    Log.enableFailQuick(false);
+    MontiArcMill.reset();
+    MontiArcMill.init();
+    BasicSymbolsMill.initializePrimitives();
+    setUpComponents();
+  }
 
   @Override
-  @BeforeEach
   public void setUp() {
-    super.setUp();
   }
 
-  @Override
-  protected String getPackage() {
-    return PACKAGE;
+  @AfterEach
+  public void tearDown() {
+    Log.clearFindings();
   }
 
-  @Override
-  protected void registerCoCos(@NotNull MontiArcCoCoChecker checker) {
-    Preconditions.checkNotNull(checker);
-    checker.addCoCo(new SubPortsConnected());
+  protected static void setUpComponents() {
+    compile("package a.b; component A { }");
+    compile("package a.b; component B { port in int i; }");
+    compile("package a.b; component C { port out int o; }");
+    compile("package a.b; component D { port in int i; port out int o; }");
+    compile("package a.b; component E { port in int i1, i2; }");
+    compile("package a.b; component F { port out int o1, o2; }");
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"AllSubPortsConnected.arc"})
-  public void shouldApproveValidPortConnections(@NotNull String model) {
+  @ValueSource(strings = {
+    "component Comp1 { }",
+    "component Comp2 { " +
+      "a.b.A a; " +
+      "}",
+    "component Comp3 { " +
+      "port in int i; " +
+      "a.b.B b; " +
+      "i -> b.i; " +
+      "}",
+    "component Comp4 { " +
+      "port out int o; " +
+      "a.b.C c; " +
+      "c.o -> o; " +
+      "}",
+    "component Comp5 { " +
+      "port out int o1, o2; " +
+      "a.b.C c; " +
+      "c.o -> o1, o2; " +
+      "}",
+    "component Comp6 { " +
+      "port in int i; " +
+      "port out int o; " +
+      "a.b.D d; " +
+      "i -> d.i; " +
+      "d.o -> o; " +
+      "}",
+    "component Comp7 { " +
+      "port in int i; " +
+      "a.b.E e; " +
+      "i -> e.i1; " +
+      "i -> e.i2; " +
+      "}",
+    "component Comp8 { " +
+      "port in int i; " +
+      "a.b.E e; " +
+      "i -> e.i1, e.i2; " +
+      "}",
+    "component Comp9 { " +
+      "port out int o1, o2; " +
+      "a.b.F f; " +
+      "f.o1 -> o1; " +
+      "f.o2 -> o2; " +
+      "}",
+    "component Comp10 { " +
+      "port in int i; " +
+      "port out int o; " +
+      "component Inner {" +
+      "port in int i; " +
+      "port out int o; " +
+      "}" +
+      "Inner sub; " +
+      "i -> sub.i; " +
+      "sub.o -> o; " +
+      "}"
+  })
+  public void shouldNotReportError(@NotNull String model) throws IOException {
     Preconditions.checkNotNull(model);
 
-    //Given
-    ASTMACompilationUnit ast = this.parseAndCreateAndCompleteSymbols(model);
+    // Given
+    ASTMACompilationUnit ast = MontiArcMill.parser().parse_StringMACompilationUnit(model).orElseThrow();
+    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
+    MontiArcMill.symbolTableCompleterDelegator().createFromAST(ast);
 
-    //When
-    this.getChecker().checkAll(ast);
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
+    checker.addCoCo(new SubPortsConnected());
 
-    //Then
-    Assertions.assertEquals(0, Log.getFindingsCount());
-  }
+    // When
+    checker.checkAll(ast);
 
-  protected static Stream<Arguments> modelAndExpectedErrorsProvider() {
-    return Stream.of(
-            arg("SubIncomingPortNotConnected.arc",
-                    ArcError.IN_PORT_NOT_CONNECTED, ArcError.IN_PORT_NOT_CONNECTED),
-            arg("SubOutgoingPortNotConnected.arc",
-                    ArcError.OUT_PORT_NOT_CONNECTED, ArcError.OUT_PORT_NOT_CONNECTED),
-            arg("MissingSubPortConnections.arc",
-                    ArcError.OUT_PORT_NOT_CONNECTED, ArcError.OUT_PORT_NOT_CONNECTED,
-                    ArcError.IN_PORT_NOT_CONNECTED, ArcError.IN_PORT_NOT_CONNECTED)
-    );
+    // Then
+    Assertions.assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
   }
 
   @ParameterizedTest
-  @MethodSource("modelAndExpectedErrorsProvider")
-  public void shouldFindInvalidTypes(@NotNull String model, @NotNull Error... errors) {
+  @MethodSource("invalidModels")
+  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
-    //Given
-    ASTMACompilationUnit ast = this.parseAndCreateSymbols(model);
+    // Given
+    ASTMACompilationUnit ast = MontiArcMill.parser().parse_StringMACompilationUnit(model).orElseThrow();
+    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
+    MontiArcMill.symbolTableCompleterDelegator().createFromAST(ast);
 
-    //When
-    this.getChecker().checkAll(ast);
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
+    checker.addCoCo(new SubPortsConnected());
 
-    //Then
-    this.checkOnlyExpectedErrorsPresent(errors);
+    // When
+    checker.checkAll(ast);
+
+    // Then
+    Assertions.assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
+    Assertions.assertThat(this.collectErrorCodes(Log.getFindings())).as(Log.getFindings().toString())
+      .containsExactlyElementsOf(this.collectErrorCodes(errors));
+  }
+
+  protected static Stream<Arguments> invalidModels() {
+    return Stream.of(
+      arg("component Comp1 { " +
+          "a.b.B b; " +
+          "}",
+        ArcError.IN_PORT_NOT_CONNECTED),
+      arg("component Comp2 { " +
+          "a.b.C c; " +
+          "}",
+        ArcError.OUT_PORT_NOT_CONNECTED),
+      arg("component Comp3 { " +
+          "a.b.D d; " +
+          "}",
+        ArcError.IN_PORT_NOT_CONNECTED,
+        ArcError.OUT_PORT_NOT_CONNECTED),
+      arg("component Comp4 { " +
+          "a.b.B b; " +
+          "a.b.C c; " +
+          "}",
+        ArcError.IN_PORT_NOT_CONNECTED,
+        ArcError.OUT_PORT_NOT_CONNECTED),
+      arg("component Comp5 { " +
+          "a.b.E e; " +
+          "}",
+        ArcError.IN_PORT_NOT_CONNECTED,
+        ArcError.IN_PORT_NOT_CONNECTED),
+      arg("component Comp6 { " +
+          "a.b.F f; " +
+          "}",
+        ArcError.OUT_PORT_NOT_CONNECTED,
+        ArcError.OUT_PORT_NOT_CONNECTED),
+      arg("component Comp7 { " +
+          "a.b.B b1, b2; " +
+          "}",
+        ArcError.IN_PORT_NOT_CONNECTED,
+        ArcError.IN_PORT_NOT_CONNECTED),
+      arg("component Comp8 { " +
+          "a.b.C c1, c2; " +
+          "}",
+        ArcError.OUT_PORT_NOT_CONNECTED,
+        ArcError.OUT_PORT_NOT_CONNECTED)
+    );
   }
 }
-
