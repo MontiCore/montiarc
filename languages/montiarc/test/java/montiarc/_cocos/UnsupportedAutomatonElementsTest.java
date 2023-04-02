5333184b054ad5813a4836c1b1b471e745c20e37
David Schmalzing <schmalzing@se-rwth.de>
2023-04-02 22:44:54 +0200

    Rework unsupported automaton elements tests

--- languages/montiarc/test/java/montiarc/_cocos/UnsupportedAutomatonElementsTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/UnsupportedAutomatonElementsTest.java
@@ -2,51 +2,113 @@
 package montiarc._cocos;
 
 import com.google.common.base.Preconditions;
+import de.se_rwth.commons.logging.Log;
+import montiarc.MontiArcAbstractTest;
+import montiarc.MontiArcMill;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.ArcError;
 import montiarc.util.Error;
+import org.assertj.core.api.Assertions;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.IOException;
 import java.util.stream.Stream;
 
-public class UnsupportedAutomatonElementsTest extends AbstractCoCoTest {
+/**
+ * The class under test is {@link UnsupportedAutomatonElements}.
+ */
+public class UnsupportedAutomatonElementsTest extends MontiArcAbstractTest {
 
-  protected final static String PACKAGE = "unsupportedAutomatonElements";
+  @ParameterizedTest
+  @ValueSource(strings = {
+    // no automaton
+    "component Comp1 { }",
+    // no states
+    "component Comp2 {" +
+      "automaton { } " +
+      "}",
+    // no final state
+    "component Comp3 { " +
+      "automaton { " +
+      "initial state s1; " +
+      "state s2; " +
+      "state s3; " +
+      "} " +
+      "}"
+  })
+  public void shouldNotReportError(@NotNull String model) throws IOException {
+    Preconditions.checkNotNull(model);
 
-  @Override
-  protected String getPackage() {
-    return PACKAGE;
-  }
+    // Given
+    ASTMACompilationUnit ast = MontiArcMill.parser().parse_StringMACompilationUnit(model).orElseThrow();
+    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
+    MontiArcMill.symbolTableCompleterDelegator().createFromAST(ast);
 
-  @Override
-  protected void registerCoCos(@NotNull MontiArcCoCoChecker checker) {
-    Preconditions.checkNotNull(checker);
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new UnsupportedAutomatonElements.FinalStates());
-  }
 
+    // When
+    checker.checkAll(ast);
 
-  protected static Stream<Arguments> modelAndExpectedErrorsProvider() {
-    return Stream.of(
-      arg("HasFinalState.arc", ArcError.UNSUPPORTED_MODEL_ELEMENT)
-    );
+    // Then
+    Assertions.assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
   }
 
   @ParameterizedTest
-  @MethodSource("modelAndExpectedErrorsProvider")
-  protected void shouldFindUnsupportedAutomatonElements(@NotNull String model, @NotNull Error... errors) {
+  @MethodSource("invalidModels")
+  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
-    //Given
-    ASTMACompilationUnit ast = this.parseAndCreateSymbols(model);
+    // Given
+    ASTMACompilationUnit ast = MontiArcMill.parser().parse_StringMACompilationUnit(model).orElseThrow();
+    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
+    MontiArcMill.symbolTableCompleterDelegator().createFromAST(ast);
 
-    //When
-    this.getChecker().checkAll(ast);
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
+    checker.addCoCo(new UnsupportedAutomatonElements.FinalStates());
 
-    //Then
-    this.checkOnlyExpectedErrorsPresent(errors);
+    // When
+    checker.checkAll(ast);
+
+    // Then
+    Assertions.assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
+    Assertions.assertThat(this.collectErrorCodes(Log.getFindings())).as(Log.getFindings().toString())
+      .containsExactlyElementsOf(this.collectErrorCodes(errors));
+  }
+
+  protected static Stream<Arguments> invalidModels() {
+    return Stream.of(
+      // one final state
+      arg("component Comp1 { " +
+          "automaton { " +
+          "final state s; " +
+          "} " +
+          "}",
+        ArcError.UNSUPPORTED_MODEL_ELEMENT),
+      // multiple states, one final state
+      arg("component Comp2 { " +
+          "automaton { " +
+          "initial state s1; " +
+          "state s2; " +
+          "state s3; " +
+          "final state s4; " +
+          "} " +
+          "}",
+        ArcError.UNSUPPORTED_MODEL_ELEMENT),
+      // multiple final states
+      arg("component Comp3 { " +
+          "automaton { " +
+          "final state s1; " +
+          "final state s2; " +
+          "} " +
+          "}",
+        ArcError.UNSUPPORTED_MODEL_ELEMENT,
+        ArcError.UNSUPPORTED_MODEL_ELEMENT)
+    );
   }
 }
