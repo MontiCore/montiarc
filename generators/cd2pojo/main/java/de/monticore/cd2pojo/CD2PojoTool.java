02a19973c4a83f825b821176767815c095df893a
David Schmalzing <schmalzing@se-rwth.de>
2023-09-21 13:54:43 +0200

    Fix class cast exception, only store type symbols

--- generators/cd2pojo/main/java/de/monticore/cd2pojo/CD2PojoTool.java
+++ generators/cd2pojo/main/java/de/monticore/cd2pojo/CD2PojoTool.java
@@ -6,6 +6,7 @@ import de.monticore.cd4analysis._symboltable.ICD4AnalysisScope;
 import de.monticore.cd4code.CD4CodeMill;
 import de.monticore.cd4code._symboltable.ICD4CodeArtifactScope;
 import de.monticore.cdbasis._symboltable.CDTypeSymbol;
+import de.monticore.symboltable.ISymbol;
 import de.se_rwth.commons.Names;
 
 public class CD2PojoTool extends CDGeneratorTool {
@@ -19,11 +20,14 @@ public class CD2PojoTool extends CDGeneratorTool {
   public void storeSymTab(ICD4CodeArtifactScope scope, String path) {
     for (ICD4AnalysisScope subscope : scope.getSubScopes()) {
       // store each type symbol in its own symbol-table
-      ICD4CodeArtifactScope as2store = CD4CodeMill.artifactScope();
-      as2store.setPackageName(Names.getQualifiedName(scope.getPackageName(), scope.getName()));
-      as2store.setName(subscope.getName());
-      as2store.add((CDTypeSymbol) subscope.getSpanningSymbol());
-      super.storeSymTab(as2store, path);
+      ISymbol symbol = subscope.getSpanningSymbol();
+      if (symbol instanceof CDTypeSymbol) {
+        ICD4CodeArtifactScope as2store = CD4CodeMill.artifactScope();
+        as2store.setPackageName(Names.getQualifiedName(scope.getPackageName(), scope.getName()));
+        as2store.setName(subscope.getName());
+        as2store.add((CDTypeSymbol) subscope.getSpanningSymbol());
+        super.storeSymTab(as2store, path);
+      }
     }
   }
 }
