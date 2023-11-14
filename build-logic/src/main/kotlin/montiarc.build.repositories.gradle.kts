507654a49dd37917097ed579ac595095d790f5d1
David Schmalzing <schmalzing@se-rwth.de>
2023-11-14 11:18:27 +0100

    Add maven local option

--- build-logic/src/main/kotlin/montiarc.build.repositories.gradle.kts
+++ build-logic/src/main/kotlin/montiarc.build.repositories.gradle.kts
@@ -1,8 +1,12 @@
 /* (c) https://github.com/MontiCore/monticore */
+import java.lang.System.getProperty
 
 val repo: String = "https://nexus.se.rwth-aachen.de/content/groups/public/"
 
 repositories {
+  if ("true" == getProperty("useLocalRepo")) {
+    mavenLocal()
+  }
   maven {
     url = uri(repo)
   }
