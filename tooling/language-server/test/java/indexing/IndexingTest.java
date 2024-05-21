94fc29b18ae0ab426a7d586f08c75e9e61e3ca07
Alexander Hellwig <hellwig@se-rwth.de>
2024-05-21 11:21:25 +0200

    Fix IndexingTest

--- tooling/language-server/test/java/indexing/IndexingTest.java
+++ tooling/language-server/test/java/indexing/IndexingTest.java
@@ -5,6 +5,7 @@ import de.mclsg.lsp.util.AsyncUtilWithSyncExec;
 import de.monticore.io.paths.MCPath;
 import de.monticore.symbols.compsymbols._symboltable.SubcomponentSymbol;
 import montiarc._lsp.MontiArcLanguageServerBuilder;
+import org.eclipse.lsp4j.InitializedParams;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import util.MockLanguageClient;
@@ -14,13 +15,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IndexingTest {
 
   @Test
-  @Disabled
   public void testTypesAfterIndexing(){
     AsyncUtilWithSyncExec.init();
     var languageServer = new MontiArcLanguageServerBuilder()
         .modelPath(new MCPath("test/resources/indexing/")).build();
 
     languageServer.connect(new MockLanguageClient());
+    languageServer.initialized(new InitializedParams());
 
     languageServer.getLanguageAccess().getScopeManager().syncAccessGlobalScope(globalScope -> {
       assertEquals(2, globalScope.getSubScopes().size());
