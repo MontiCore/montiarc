678356da86f59fe6e9d9ddb433f6435f4365341d
David Schmalzing <schmalzing@se-rwth.de>
2024-08-28 23:52:16 +0200

    Formatting and layouting

--- languages/conformance/build.gradle.kts
+++ languages/conformance/build.gradle.kts
@@ -1,49 +1,47 @@
 /* (c) https://github.com/MontiCore/monticore */
 
 plugins {
-    id("montiarc.build.language")
-    id("montiarc.build.java-library")
-    id("montiarc.build.shadow")
+  id("montiarc.build.language")
+  id("montiarc.build.java-library")
+  id("montiarc.build.shadow")
 }
-
 
 buildDir = file(project(":languages").buildDir.toString() + "/${project.name}")
 
 dependencies {
-    grammar(libs.mc.grammar)
-    grammar(libs.mc.sc)
+  grammar(libs.mc.grammar)
+  grammar(libs.mc.sc)
 
-    api(project(":languages:montiarc"))
-    api(project(":languages:basis"))
-    api(libs.mc.sc) {
-        exclude("org.apache.groovy", "groovy")
-    }
+  api(project(":languages:montiarc"))
+  api(project(":languages:basis"))
+  api(libs.mc.sc) {
+    exclude("org.apache.groovy", "groovy")
+  }
 
-    implementation(libs.apache)
-    implementation(libs.guava)
-    implementation(libs.z3)
-    implementation(libs.mc.cd4a)
-    implementation(libs.mc.ocl.ocl2smt)
-    implementation(variantOf(libs.mc.cd4a) {classifier("cd2smt")})
+  implementation(libs.apache)
+  implementation(libs.guava)
+  implementation(libs.z3)
+  implementation(libs.mc.cd4a)
+  implementation(libs.mc.ocl.ocl2smt)
+  implementation(variantOf(libs.mc.cd4a) { classifier("cd2smt") })
 }
 
 tasks.shadowJar {
-    manifest {
-        attributes["Main-Class"] = "montiarc.conformance.AutConformanceTool"
-    }
-    isZip64 = true
-    archiveClassifier.set("mc-tool")
-    archiveBaseName.set("MAConformance")
-    archiveFileName.set( "${archiveBaseName.get()}.${archiveExtension.get()}" )
+  manifest {
+    attributes["Main-Class"] = "montiarc.conformance.AutConformanceTool"
+  }
+  isZip64 = true
+  archiveClassifier.set("mc-tool")
+  archiveBaseName.set("MAConformance")
+  archiveFileName.set("${archiveBaseName.get()}.${archiveExtension.get()}")
 }
 
-
 tasks.register<JavaExec>("runDemo") {
-    classpath = files(tasks.shadowJar.get().archiveFile)
-    val modelDir = "test/resources/montiarc/conformance/demo/"
-    args = listOf(
-        "--reference", modelDir+"Reference.arc", modelDir+"Reference.cd",
-        "--concrete", modelDir+"Concrete.arc", modelDir+"Concrete.cd",
-        "--map", modelDir+"Mapping.map"
-    )
+  classpath = files(tasks.shadowJar.get().archiveFile)
+  val modelDir = "test/resources/montiarc/conformance/demo/"
+  args = listOf(
+      "--reference", modelDir + "Reference.arc", modelDir + "Reference.cd",
+      "--concrete", modelDir + "Concrete.arc", modelDir + "Concrete.cd",
+      "--map", modelDir + "Mapping.map"
+  )
 }
--- languages/effect/build.gradle.kts
+++ languages/effect/build.gradle.kts
@@ -1,67 +1,65 @@
 /* (c) https://github.com/MontiCore/monticore */
 
-
-
 plugins {
-    id("montiarc.build.language")
-    id("montiarc.build.java-library")
-    id("montiarc.build.shadow")
+  id("montiarc.build.language")
+  id("montiarc.build.java-library")
+  id("montiarc.build.shadow")
 }
 
 buildDir = file(project(":languages").buildDir.toString() + "/${project.name}")
 
 dependencies {
-    grammar(libs.mc.grammar)
-    grammar(libs.mc.sc)
+  grammar(libs.mc.grammar)
+  grammar(libs.mc.sc)
 
-    api(project(":languages:montiarc"))
-    api(project(":languages:basis"))
-    api(project(":languages:conformance"))
-    api(libs.mc.sc) {
-        exclude("org.apache.groovy", "groovy")
-    }
+  api(project(":languages:montiarc"))
+  api(project(":languages:basis"))
+  api(project(":languages:conformance"))
+  api(libs.mc.sc) {
+    exclude("org.apache.groovy", "groovy")
+  }
 
-    implementation(libs.apache)
-    implementation(libs.jgrapht.core)
-    implementation(libs.jgrapht.jgraphx)
-    implementation(libs.jgrapht.ext)
-    implementation(libs.guava)
-    implementation(libs.z3)
-    implementation(libs.mc.cd4a)
-    implementation(libs.mc.ocl.ocl2smt)
-    implementation(variantOf(libs.mc.cd4a) {classifier("cd2smt")})
+  implementation(libs.apache)
+  implementation(libs.jgrapht.core)
+  implementation(libs.jgrapht.jgraphx)
+  implementation(libs.jgrapht.ext)
+  implementation(libs.guava)
+  implementation(libs.z3)
+  implementation(libs.mc.cd4a)
+  implementation(libs.mc.ocl.ocl2smt)
+  implementation(variantOf(libs.mc.cd4a) { classifier("cd2smt") })
 }
 
 tasks.shadowJar {
-    manifest {
-        attributes["Main-Class"] = "mceffect.MCEffectTool"
-    }
-    isZip64 = true
-    archiveClassifier.set("mc-tool")
-    archiveBaseName.set("MCEffect")
-    archiveFileName.set( "${archiveBaseName.get()}.${archiveExtension.get()}" )
+  manifest {
+    attributes["Main-Class"] = "mceffect.MCEffectTool"
+  }
+  isZip64 = true
+  archiveClassifier.set("mc-tool")
+  archiveBaseName.set("MCEffect")
+  archiveFileName.set("${archiveBaseName.get()}.${archiveExtension.get()}")
 }
 
 tasks.register<JavaExec>("runSteamboilderDemo") {
-    classpath = files(tasks.shadowJar.get().archiveFile)
-    val modelDir = "test/resources/mceffect/demo/"
+  classpath = files(tasks.shadowJar.get().archiveFile)
+  val modelDir = "test/resources/mceffect/demo/"
 
-    args = listOf(
-        "-mp", modelDir+"steamboiler/",
-        "-mc", "demo.steamboiler.SteamBoiler",
-        "-e",  "SteamBoiler.eff"
-        // ,"-g"   // comment in to show graph
-    )
+  args = listOf(
+      "-mp", modelDir + "steamboiler/",
+      "-mc", "demo.steamboiler.SteamBoiler",
+      "-e", "SteamBoiler.eff"
+      // ,"-g"   // comment in to show graph
+  )
 }
 
 tasks.register<JavaExec>("runDemo") {
-    classpath = files(tasks.shadowJar.get().archiveFile)
-    val modelDir = "test/mceffect/demo/"
+  classpath = files(tasks.shadowJar.get().archiveFile)
+  val modelDir = "test/mceffect/demo/"
 
-    args = listOf(
-        "-mp", modelDir+"runningexample/",
-        "-mc", "demo.runningexample.Comp0",
-        "-e", "Comp0.eff"
-        // ,"-g"   // comment in to show graph
-    )
+  args = listOf(
+      "-mp", modelDir + "runningexample/",
+      "-mc", "demo.runningexample.Comp0",
+      "-e", "Comp0.eff"
+      // ,"-g"   // comment in to show graph
+  )
 }
