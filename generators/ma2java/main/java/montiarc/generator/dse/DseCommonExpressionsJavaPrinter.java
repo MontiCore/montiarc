2eacca487b9f293583e9e9a7a0216e56e3524d0b
David Schmalzing <schmalzing@se-rwth.de>
2025-05-16 13:22:57 +0200

    Remove redundant ovverride, fix deprecation

--- generators/ma2java/main/java/montiarc/generator/dse/DseCommonExpressionsJavaPrinter.java
+++ generators/ma2java/main/java/montiarc/generator/dse/DseCommonExpressionsJavaPrinter.java
@@ -1,19 +1,12 @@
 /* (c) https://github.com/MontiCore/monticore */
 package montiarc.generator.dse;
 
-import com.google.common.base.Preconditions;
-import de.monticore.expressions.commonexpressions._ast.ASTCallExpression;
-import de.monticore.expressions.commonexpressions._ast.ASTFieldAccessExpression;
 import de.monticore.expressions.commonexpressions._prettyprint.CommonExpressionsPrettyPrinter;
 import de.monticore.prettyprint.CommentPrettyPrinter;
 import de.monticore.prettyprint.IndentPrinter;
-import de.monticore.symbols.oosymbols._symboltable.MethodSymbol;
-import de.monticore.symboltable.ISymbol;
 import montiarc.generator.helper.ComponentHelper;
 import montiarc.generator.helper.dse.ComponentHelperDseValue;
 import org.codehaus.commons.nullanalysis.NotNull;
-
-import java.util.Optional;
 
 public class DseCommonExpressionsJavaPrinter extends CommonExpressionsPrettyPrinter {
   ComponentHelperDseValue componentHelperDseValue;
@@ -23,35 +16,6 @@ public class DseCommonExpressionsJavaPrinter extends CommonExpressionsPrettyPrin
     super(printer, printComments);
     this.componentHelperDseValue = new ComponentHelperDseValue();
     this.componentHelper = new ComponentHelper();
-  }
-
-  @Override
-  public void handle(@NotNull ASTCallExpression node) {
-    Preconditions.checkNotNull(node);
-    if (this.isPrintComments()) {
-      CommentPrettyPrinter.printPreComments(node, getPrinter());
-    }
-
-    Optional<ISymbol> symbol = node.getDefiningSymbol();
-    if (symbol.isPresent()
-      && symbol.get() instanceof MethodSymbol
-      && ((MethodSymbol) symbol.get()).isIsConstructor()
-    ) {
-      Preconditions.checkState(node.getExpression() instanceof ASTFieldAccessExpression);
-      this.getPrinter().print("new ");
-      // At the moment we need to call the Name of the class to instantiate as a qualification
-      // before the constructor
-      // call in MontiArc models. But Java does not do this, so we have to strip this qualification.
-      // See https://git.rwth-aachen.de/monticore/montiarc/core/-/merge_requests/677
-      ((ASTFieldAccessExpression) node.getExpression()).getExpression().accept(getTraverser());
-    }
-    else {
-      node.getExpression().accept(getTraverser());
-    }
-    node.getArguments().accept(getTraverser());
-    if (this.isPrintComments()) {
-      CommentPrettyPrinter.printPostComments(node, getPrinter());
-    }
   }
 
   @Override
