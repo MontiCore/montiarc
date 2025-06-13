e864e4dd1e43644551da9fec2ee6251624941121
David Pascal Schmalzing <schmalzing@se-rwth.de>
2025-06-13 13:43:02 +0200

    Update to java 17

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -47,6 +47,6 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_17
 }
