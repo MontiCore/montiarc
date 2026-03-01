b359cc70231638f6b6f983f97f6707826e09840f
David Schmalzing <schmalzing@se-rwth.de>
2026-03-01 12:54:43 +0100

    Rework connector timings fit tests

--- languages/montiarc/test/java/montiarc/_cocos/ConnectorTimingsFitTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/ConnectorTimingsFitTest.java
@@ -1,210 +1,47 @@
 /* (c) https://github.com/MontiCore/monticore */
 package montiarc._cocos;
 
+import arcbasis._cocos.ConnectorDirectionsFit;
 import arcbasis._cocos.ConnectorTimingsFit;
 import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
-import montiarc.MontiArcMill;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcError;
+import montiarc.util.Error;
 import org.codehaus.commons.nullanalysis.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
+import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.CONNECTOR_TIMING_MISMATCH;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The class under test is {@link ConnectorTimingsFit}.
  */
-public class ConnectorTimingsFitTest extends MontiArcTestBase {
+class ConnectorTimingsFitTest extends MontiArcTestBase {
+
+  @BeforeEach
+  protected void setUp() {
+    compile("package a.b; component A { }");
+    compile("package a.b; component B { port in int i, out int o; }");
+    compile("package a.b; component C { port sync in int i, sync out int o; }");
+  }
 
   @ParameterizedTest
-  @ValueSource(strings = {
-    // all timing match (sync)
-    "component c1 {"
-      + "port sync in int i;"
-      + "port sync out int o;"
-      + "component Inner {"
-      + "port sync in int i;"
-      + "port sync out int o;"
-      + "}"
-      + "Inner inner1, inner2;"
-      + "i -> inner1.i;"
-      + "inner1.o -> inner2.i;"
-      + "inner2.o -> o;"
-      + "}",
-    // all timing match (timed)
-    "component c2 {"
-      + "port in int i;"
-      + "port out int o;"
-      + "component Inner {"
-      + "port in int i;"
-      + "port out int o;"
-      + "}"
-      + "Inner inner1, inner2;"
-      + "i -> inner1.i;"
-      + "inner1.o -> inner2.i;"
-      + "inner2.o -> o;"
-      + "}",
-    // all timing match (timed)
-    "component c3 {"
-      + "port in int i;"
-      + "port out int o;"
-      + "component Inner {"
-      + "port in int i;"
-      + "port out int o;"
-      + "}"
-      + "Inner inner;"
-      + "i -> inner.i;"
-      + "inner.o -> o;"
-      + "}",
-    // all timing match (sync)
-    "component c4 {"
-      + "port sync in int i;"
-      + "port sync out int o;"
-      + "component Inner {"
-      + "port sync in int i;"
-      + "port sync out int o;"
-      + "}"
-      + "Inner inner;"
-      + "i -> inner.i;"
-      + "inner.o -> o;"
-      + "}",
-    // all timing match (sync) - multiple targets
-    "component c5 {"
-      + "port sync in int i;"
-      + "port sync out int o1;"
-      + "port sync out int o2;"
-      + "component Inner {"
-      + "port sync in int i1;"
-      + "port sync in int i2;"
-      + "port sync out int o;"
-      + "}"
-      + "Inner inner;"
-      + "i -> inner.i1;"
-      + "i -> inner.i2;"
-      + "inner.o -> o1;"
-      + "inner.o -> o2;"
-      + "}",
-    // all timings match - pass through connector
-    "component c6 {"
-      + "port in int i;"
-      + "port out int o;"
-      + "i -> o;"
-      + "component Inner { }"
-      + "Inner inner; "
-      + "}",
-    // all timings match for an input port forward (sync -> timed)
-    "component c7 {"
-      + "port sync in int i;"
-      + "component Inner {"
-      + "port in int i;"
-      + "}"
-      + "Inner inner;"
-      + "i -> inner.i;"
-      + "}",
-    // all timings match for an output port forward (sync -> timed)
-    "component c8 {"
-      + "port out int o;"
-      + "component Inner {"
-      + "port sync out int o;"
-      + "}"
-      + "Inner inner;"
-      + "inner.o -> o;"
-      + "}",
-    // all timings match for a pass through connector (sync -> timed)
-    "component c9 {"
-      + "port sync in int i;"
-      + "port out int o;"
-      + "i -> o;"
-      + "}",
-    // all timings match for a hidden connector (sync -> timed)
-    "component c10 {"
-      + "component Inner {"
-      + "port in int i;"
-      + "port sync out int o;"
-      + "}"
-      + "Inner inner1, inner2;"
-      + "inner1.o -> inner2.i;" // sync -> timed
-      + "}",
-    // all timings match for an input port forward (sync -> timed) - multiple targets
-    "component c11 {"
-      + "port sync in int i;"
-      + "component Inner {"
-      + "port sync in int i1;"
-      + "port in int i2;"
-      + "}"
-      + "Inner inner;"
-      + "i -> inner.i1;"
-      + "i -> inner.i2;"
-      + "}",
-    // all timings match for an output port forward (sync -> timed) - multiple targets
-    "component c12 {"
-      + "port sync out int o1;"
-      + "port out int o2;"
-      + "component Inner {"
-      + "port sync out int o;"
-      + "}"
-      + "Inner inner;"
-      + "inner.o -> o1;"
-      + "inner.o -> o2;"
-      + "}",
-    // all timings match for a hidden connector (sync -> timed) - multiple targets
-    "component c13 {"
-      + "component Source {"
-      + "port sync out int o;"
-      + "}"
-      + "component Sink {"
-      + "port sync in int i1;"
-      + "port in int i2;"
-      + "}"
-      + "Source source;"
-      + "Sink sink;"
-      + "source.o -> sink.i1;"
-      + "source.o -> sink.i2;"
-      + "}",
-    // mismatched timing for a hidden connector (sync -> untimed)
-    // the automaton defines the timing for the outgoing port
-    "component c14 {"
-      + "component Source {"
-      + "port sync out int o;"
-      + "}"
-      + "component Sink {"
-      + "port in int i;"
-      + "automaton { }"
-      + "}"
-      + "Source source;"
-      + "Sink sink;"
-      + "source.o -> sink.i;"
-      + "}",
-    // mismatched timing for an output port forward (sync -> untimed)
-    // automaton override for incoming port
-    "component c15 {"
-      + "port out int o;"
-      + "component Inner {"
-      + "port in int i;"
-      + "port sync out int o;"
-      + "automaton { }"
-      + "}"
-      + "Inner inner;"
-      + "inner.o -> o;"
-      + "}",
-  })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  @MethodSource("validModels")
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
-    ASTMACompilationUnit ast = MontiArcMill.parser()
-      .parse_StringMACompilationUnit(model).orElseThrow();
-    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
-    MontiArcMill.scopesGenitorP2Delegator().createFromAST(ast);
-    MontiArcMill.scopesGenitorP3Delegator().createFromAST(ast);
+    ASTMACompilationUnit ast = compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new ConnectorTimingsFit());
+    checker.addCoCo(new ConnectorDirectionsFit());
 
     // When
     checker.checkAll(ast);
@@ -214,75 +51,230 @@ public class ConnectorTimingsFitTest extends MontiArcTestBase {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {
-    // mismatched timing for an output port forward (timed -> sync)
-    "component c1 {"
-      + "port sync out int o;"
-      + "component Inner {"
-      + "port out int o;"
-      + "}"
-      + "Inner inner;"
-      + "inner.o -> o;"
-      + "}",
-    // mismatched timing for a hidden connector (timed -> sync)
-    "component c2 {"
-      + "component Inner {"
-      + "port sync in int i;"
-      + "port out int o;"
-      + "}"
-      + "Inner inner1, inner2;"
-      + "inner1.o -> inner2.i;" // timed -> sync
-      + "}",
-    // mismatched timing for a hidden connector (untimed -> sync)
-    "component c3 {"
-      + "component Source {"
-      + "port out int o;"
-      + "}"
-      + "component Sink {"
-      + "port sync in int i;"
-      + "}"
-      + "Source source;"
-      + "Sink sink;"
-      + "source.o -> sink.i;"
-      + "}",
-    // mismatched timing for an input port forward (untimed -> sync)
-    "component c4 {"
-      + "port in int i;"
-      + "component Inner {"
-      + "port sync in int i;"
-      + "port out int o;"
-      + "}"
-      + "Inner inner;"
-      + "i -> inner.i;"
-      + "}",
-    // mismatched timing for a pass through connector with default source timing (timed -> sync)
-    "component c5 {"
-      + "port in int i;"
-      + "port sync out int o;"
-      + "i -> o;"
-      + "component Inner { }"
-      + "Inner inner; "
-      + "}"
-  })
-  public void shouldReportError(@NotNull String model) throws IOException {
+  @MethodSource("invalidModels")
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
+    Preconditions.checkNotNull(errors);
 
     // Given
-    ASTMACompilationUnit ast = MontiArcMill.parser()
-      .parse_StringMACompilationUnit(model).orElse(null);
-    Preconditions.checkNotNull(ast);
-    MontiArcMill.scopesGenitorDelegator().createFromAST(ast);
-    MontiArcMill.scopesGenitorP2Delegator().createFromAST(ast);
-    MontiArcMill.scopesGenitorP3Delegator().createFromAST(ast);
+    ASTMACompilationUnit ast = compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new ConnectorTimingsFit());
+    checker.addCoCo(new ConnectorDirectionsFit());
 
     // When
     checker.checkAll(ast);
 
     // Then
     assertThat(getLoggedErrorCodes())
-      .containsExactlyInAnyOrder(getErrorCodes(ArcError.CONNECTOR_TIMING_MISMATCH));
+      .containsExactlyInAnyOrder(getErrorCodes(errors));
+  }
+
+  protected static Stream<Arguments> validModels() {
+    return Stream.of(
+      // decomposed component, no ports or connectors
+      arg("""
+        component ValidComp1 {
+          a.b.A sub;
+        }"""
+      ),
+      // input forward
+      arg("""
+        component ValidComp2 {
+          port in int i;
+          a.b.B sub;
+          i -> sub.i;
+        }"""
+      ),
+      // sync input forward
+      arg("""
+        component ValidComp3 {
+          port sync in int i;
+          a.b.C sub;
+          i -> sub.i;
+        }"""
+      ),
+      // sync (to event) input forward
+      arg("""
+        component ValidComp4 {
+          port sync in int i;
+          a.b.B sub;
+          i -> sub.i;
+        }"""
+      ),
+      // output forward
+      arg("""
+        component ValidComp5 {
+          port out int o;
+          a.b.B sub;
+          sub.o -> o;
+        }"""
+      ),
+      // sync output forward
+      arg("""
+        component ValidComp6 {
+          port sync out int o;
+          a.b.C sub;
+          sub.o -> o;
+        }"""
+      ),
+      // sync (to event) output forward
+      arg("""
+        component ValidComp7 {
+          port out int o;
+          a.b.C sub;
+          sub.o -> o;
+        }"""
+      ),
+      // hidden channel
+      arg("""
+        component ValidComp8 {
+          a.b.B sub1;
+          a.b.B sub2;
+          sub1.o -> sub2.i;
+        }"""
+      ),
+      // sync hidden channel
+      arg("""
+        component ValidComp9 {
+          a.b.C sub1;
+          a.b.C sub2;
+          sub1.o -> sub2.i;
+        }"""
+      ),
+      // sync (to event) hidden channel
+      arg("""
+        component ValidComp9 {
+          a.b.C sub1;
+          a.b.B sub2;
+          sub1.o -> sub2.i;
+        }"""
+      ),
+      // port forward, inner component
+      arg("""
+        component ValidComp10 {
+          port in int i;
+          port out int o;
+          component Inner {
+            port in int i;
+            port out int o;
+          }
+          Inner sub;
+          i -> sub.i;
+          sub.o -> o;
+        }"""
+      ),
+      // sync port forward, inner component
+      arg("""
+        component ValidComp11 {
+          port sync in int i;
+          port sync out int o;
+          component Inner {
+            port sync in int i;
+            port sync out int o;
+          }
+          Inner sub;
+          i -> sub.i;
+          sub.o -> o;
+        }"""
+      ),
+      // message pass through
+      arg("""
+        component ValidComp12 {
+          port in int i;
+          port out int o;
+          i -> o;
+        }"""
+      ),
+      // sync message pass through
+      arg("""
+        component ValidComp13 {
+          port sync in int i;
+          port sync out int o;
+          i -> o;
+        }"""
+      ),
+      // sync (to event) message pass through
+      arg("""
+        component ValidComp14 {
+          port sync in int i;
+          port out int o;
+          i -> o;
+        }"""
+      )
+    );
+  }
+
+  protected static Stream<Arguments> invalidModels() {
+    return Stream.of(
+      // input forward, timing mismatch
+      arg("""
+          component InvalidComp1 {
+            port in int i;
+            a.b.C sub;
+            i -> sub.i;
+          }""",
+        CONNECTOR_TIMING_MISMATCH
+      ),
+      // output forward, timing mismatch
+      arg("""
+          component InvalidComp2 {
+            port sync out int o;
+            a.b.B sub;
+            sub.o -> o;
+          }""",
+        CONNECTOR_TIMING_MISMATCH
+      ),
+      // hidden channel, timing mismatch
+      arg("""
+          component InvalidComp3 {
+            a.b.B sub1;
+            a.b.C sub2;
+            sub1.o -> sub2.i;
+          }
+          """,
+        CONNECTOR_TIMING_MISMATCH
+      ),
+      // input forward, timing mismatch, multiple targets
+      arg("""
+          component InvalidComp4 {
+            port in int i;
+            a.b.B sub1;
+            a.b.C sub2;
+            i -> sub1.i, sub2.i;
+          }""",
+        CONNECTOR_TIMING_MISMATCH
+      ),
+      // input forward, multiple timing mismatch, multiple targets
+      arg("""
+          component InvalidComp5 {
+            port in int i;
+            a.b.C sub1;
+            a.b.C sub2;
+            i -> sub1.i, sub2.i;
+          }""",
+        CONNECTOR_TIMING_MISMATCH, CONNECTOR_TIMING_MISMATCH
+      ),
+      // output forward, timing mismatch, multiple targets
+      arg("""
+          component InvalidComp6 {
+            port out int o1, sync out int o2;
+            a.b.B sub;
+            sub.o -> o1, o2;
+          }""",
+        CONNECTOR_TIMING_MISMATCH
+      ),
+      // output forward, multiple timing mismatch, multiple targets
+      arg("""
+          component InvalidComp6 {
+            port sync out int o1, sync out int o2;
+            a.b.B sub;
+            sub.o -> o1, o2;
+          }""",
+        CONNECTOR_TIMING_MISMATCH, CONNECTOR_TIMING_MISMATCH
+      )
+    );
   }
 }
