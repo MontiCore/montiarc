b8ecda7b6a70f637820fa9ae4e7832f579e24829
David Schmalzing <schmalzing@se-rwth.de>
2026-03-02 15:40:56 +0000

    Rework for each is valid tests

--- languages/montiarc/test/java/montiarc/_cocos/ForEachIsValid4FamilyTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/ForEachIsValid4FamilyTest.java
@@ -2,81 +2,211 @@
 package montiarc._cocos;
 
 import com.google.common.base.Preconditions;
-import de.monticore.class2mc.OOClass2MCResolver;
-import de.monticore.symbols.oosymbols._symboltable.OOTypeSymbol;
-import de.monticore.types.check.SymTypeExpressionFactory;
 import de.se_rwth.commons.logging.Log;
-import montiarc.MontiArcMill;
-import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
+import montiarc.util.Error;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junitpioneer.jupiter.params.DisableIfDisplayName;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import static montiarc.util.MCError.FOR_EACH_EXPR_NOT_ITERABLE;
+import static montiarc.util.MCError.FOR_EACH_TYPE_MISMATCH;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ForEachIsValid4FamilyTest extends MontiArcTestBase {
-
-  protected void initSymbols() {
-    MontiArcMill.globalScope().addAdaptedTypeSymbolResolver(new OOClass2MCResolver());
-    MontiArcMill.globalScope().addAdaptedOOTypeSymbolResolver(new OOClass2MCResolver());
-    setupEnums();
-    setupComponents();
-  }
-
-  protected void setupEnums() {
-    OOTypeSymbol onOffEnumType = MontiArcMill.oOTypeSymbolBuilder().setIsEnum(true).setName("OnOff").setIsPublic(true).setSpannedScope(MontiArcMill.scope()).build();
-    onOffEnumType.getSpannedScope().add(MontiArcMill.fieldSymbolBuilder().setName("ON").setIsStatic(true).setIsFinal(true).setIsPublic(true).setIsReadOnly(true).setType(SymTypeExpressionFactory.createTypeObject(onOffEnumType)).build());
-    onOffEnumType.getSpannedScope().add(MontiArcMill.fieldSymbolBuilder().setName("OFF").setIsStatic(true).setIsFinal(true).setIsPublic(true).setIsReadOnly(true).setType(SymTypeExpressionFactory.createTypeObject(onOffEnumType)).build());
-    MontiArcMill.globalScope().add(onOffEnumType);
-  }
-
-  protected void setupComponents() {
-    compile("package a.b; component A { }");
-    compile("package a.b; component B { port in int i; port out int o; }");
-    compile("package a.b; component C { port in int i; port <<delayed>> out int o; }");
-    compile("package a.b; component D { port in int i1, i2; port out int o; }");
-    compile("package a.b; component Z { a.b.A a1; feature f1;  port in int i; port out int o; varif(f1){port out int k; o -> a1.i;} }");
-  }
-
-  private static Stream<Arguments> provideUniqueSenderModel() {
-    List<Arguments> componentList = new ArrayList<>();
-    Arguments simpleModel = arg("component TestA {" +
-      "feature f1,f2;" +
-      "varif(f1){int a = 5;}" +
-      "varif(f2){double a = 5;} " +
-      "compute{" +
-      "\nfor(Object o : a){}" +
-      "}" +
-      "}");
-    componentList.add(simpleModel);
-    return componentList.stream();
-  }
+/**
+ * The class under test is {@link ForEachIsValid4Family}.
+ */
+class ForEachIsValid4FamilyTest extends ForEachIsValidTest {
 
   @ParameterizedTest
-  @MethodSource("provideUniqueSenderModel")
-  public void TestModelRuntimeMontiArcCoCos(@NotNull String model) {
-
+  @MethodSource("validModels")
+  @MethodSource("validModelsWithVariability")
+  @DisableIfDisplayName(contains = {
+    "ValidComp2 ",
+    "ValidComp3 ",
+    "ValidComp4 ",
+    "ValidComp5 ",
+    "ValidComp6 ",
+    "ValidComp7 ",
+    "ValidCompWithVariability2 ",
+    "ValidCompWithVariability3 "
+  })
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
+
+    // Given
+    ASTMACompilationUnit ast = compile(model);
+
     MontiArcFullVariantCoCoChecker checker = new MontiArcFullVariantCoCoChecker();
     checker.get4FullVariant().addCoCo(new ForEachIsValid4Family());
 
-    ASTMACompilationUnit mainAST = compile(model);
-    checker.checkAll(mainAST);
-
-    String[] test = getLoggedErrorCodes();
+    // When
+    checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getErrorCount() == 0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
-  private static <T> void addCoCoAs(T coco, Consumer<T> consumer) {
-    consumer.accept(coco);
+  @ParameterizedTest
+  @MethodSource("invalidModels")
+  @MethodSource("invalidModelsWithVariability")
+  @DisableIfDisplayName(contains = {
+    "InvalidComp3 ",
+    "InvalidComp4 ",
+    "InvalidCompWithVariability3 "
+  })
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
+    Preconditions.checkNotNull(model);
+    Preconditions.checkNotNull(errors);
+
+    // Given
+    ASTMACompilationUnit ast = compile(model);
+
+    MontiArcFullVariantCoCoChecker checker = new MontiArcFullVariantCoCoChecker();
+    checker.get4FullVariant().addCoCo(new ForEachIsValid4Family());
+
+    // When
+    checker.checkAll(ast);
+
+    // Then
+    assertThat(getLoggedErrorCodes())
+      .containsExactlyInAnyOrder(getErrorCodes(errors));
+  }
+
+  static Stream<Arguments> validModelsWithVariability() {
+    return Stream.of(
+      // for-each with expression of non-iterable, non-generic type, dead variation point
+      arg("""
+        import java.lang.Boolean;
+        component ValidCompWithVariability1 {
+          Boolean v = true;
+          varif (false) {
+            automaton {
+              initial state S;
+              S -> S / {
+                for (Boolean i : v) { }
+              }
+            }
+          }
+        }
+        """
+      ),
+      // for-each with expression of non-iterable, non-generic type, dead feature
+      arg("""
+        import java.lang.Boolean;
+        component ValidCompWithVariability2 {
+          feature f;
+          Boolean v = true;
+          varif (f) {
+            automaton {
+              initial state S;
+              S -> S / {
+                for (Boolean i : v) { }
+              }
+            }
+          }
+          constraint (!f);
+        }
+        """
+      ),
+      // for-each with conditional type
+      arg("""
+        import java.lang.Boolean;
+        import java.lang.Integer;
+        import java.util.List;
+        component ValidCompWithVariability3 {
+          feature f1;
+          feature f2;
+          varif (f1) {
+            List<Boolean> v = [ ];
+          }
+          else {
+            List<Integer> v = [ ];
+          }
+          varif (f2) {
+            automaton {
+              initial state S;
+              S -> S / {
+                for (Boolean i : v) { }
+              }
+            }
+          }
+          constraint (!(f2 && !f1));
+        }
+        """
+      )
+    );
+  }
+
+  static Stream<Arguments> invalidModelsWithVariability() {
+    return Stream.of(
+      // for-each with expression of non-iterable, non-generic type, tautological variation point
+      arg("""
+          import java.lang.Boolean;
+          component InvalidCompWithVariability1 {
+            Boolean v = true;
+            varif (true) {
+              automaton {
+                initial state S;
+                S -> S / {
+                  for (Boolean i : v) { }
+                }
+              }
+            }
+          }
+          """,
+        FOR_EACH_EXPR_NOT_ITERABLE
+      ),
+      // for-each with expression of non-iterable, non-generic type, core feature
+      arg("""
+          import java.lang.Boolean;
+          component InvalidCompWithVariability2 {
+            feature f;
+            Boolean v = true;
+            varif (f) {
+              automaton {
+                initial state S;
+                S -> S / {
+                  for (Boolean i : v) { }
+                }
+              }
+            }
+            constraint (f);
+          }
+          """,
+        FOR_EACH_EXPR_NOT_ITERABLE
+      ),
+      // for-each with mismatching conditional type
+      arg("""
+          import java.lang.Boolean;
+          import java.lang.Integer;
+          import java.util.List;
+          component InvalidCompWithVariability3 {
+            feature f1;
+            feature f2;
+            varif (f1) {
+              List<Boolean> v = [ ];
+            }
+            else {
+              List<Integer> v = [ ];
+            }
+            varif (f2) {
+              automaton {
+                initial state S;
+                S -> S / {
+                  for (Boolean i : v) { }
+                }
+              }
+            }
+          }
+          """,
+        FOR_EACH_TYPE_MISMATCH
+      )
+    );
   }
 }
--- languages/montiarc/test/java/montiarc/_cocos/ForEachIsValidTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/ForEachIsValidTest.java
@@ -8,16 +8,16 @@ import montiarc.MontiArcMill;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.Error;
-import montiarc.util.MCError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.MCError.FOR_EACH_EXPR_NOT_ITERABLE;
+import static montiarc.util.MCError.FOR_EACH_TYPE_MISMATCH;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -35,7 +35,7 @@ class ForEachIsValidTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @MethodSource("validModels")
-  void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -53,7 +53,8 @@ class ForEachIsValidTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -67,89 +68,103 @@ class ForEachIsValidTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(getLoggedErrorCodes()).containsExactlyInAnyOrder(getErrorCodes(errors));
+    assertThat(getLoggedErrorCodes())
+      .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
-  protected static Stream<Arguments> validModels() {
+  static Stream<Arguments> validModels() {
     return Stream.of(
+      // component no behavior
       arg("""
         component ValidComp1 { }
         """
       ),
+      // for-each over set
       arg("""
         import java.lang.Boolean;
-        import java.util.HashSet;
         import java.util.Set;
         component ValidComp2 {
-          Set<Boolean> v = HashSet();
+          Set<Boolean> v = { };
           automaton {
-            initial state S {
-              -> / {
-                for (Boolean i : v) { }
-              }
+            initial state S;
+            S -> S / {
+              for (Boolean i : v) { }
             }
           }
         }
         """
       ),
+      // for-each over list
       arg("""
         import java.lang.Boolean;
-        import java.util.ArrayList;
         import java.util.List;
         component ValidComp3 {
-          List<Boolean> v = ArrayList();
+          List<Boolean> v = [ ];
           automaton {
-            initial state S {
-              -> / {
-                for (Boolean i : v) { }
-              }
+            initial state S;
+            S -> S / {
+              for (Boolean i : v) { }
             }
           }
         }
         """
       ),
+      // for-each over subtype of set
       arg("""
         import java.lang.Boolean;
         import java.util.HashSet;
         component ValidComp4 {
-          HashSet<Boolean> v = HashSet();
+          HashSet<Boolean> v = HashSet.HashSet();
           automaton {
-            initial state S {
-              -> / {
-                for (Boolean i : v) { }
-              }
+            initial state S;
+            S -> S / {
+              for (Boolean i : v) { }
             }
           }
         }
         """
       ),
+      // for-each over subtype of list
       arg("""
         import java.lang.Boolean;
         import java.util.ArrayList;
         component ValidComp5 {
-          ArrayList<Boolean> v = ArrayList();
+          ArrayList<Boolean> v = ArrayList.ArrayList();
           automaton {
-            initial state S {
-              -> / {
-                for (Boolean i : v) { }
-              }
+            initial state S;
+            S -> S / {
+              for (Boolean i : v) { }
             }
           }
         }
         """
       ),
+      // for-each over collection
+      arg("""
+        import java.lang.Boolean;
+        import java.util.Collection;
+        component ValidComp6 {
+          Collection<Boolean> v = [ ];
+          automaton {
+            initial state S;
+            S -> S / {
+              for (Boolean i : v) { }
+            }
+          }
+        }
+        """
+      ),
+      // for-each with subtype
       arg("""
         import java.lang.Integer;
         import java.lang.Number;
         import java.util.Collection;
-        import java.util.Collections;
-        component ValidComp6 {
-          Collection<Integer> v = Collections.emptyList();
+        component ValidComp7 {
+          Collection<Integer> v = [ ];
           automaton {
-            initial state S {
-              -> / {
-                for (Number i : v) { }
-              }
+            initial state S;
+            S -> S / {
+              for (Number i : v) { }
             }
           }
         }
@@ -158,74 +173,74 @@ class ForEachIsValidTest extends MontiArcTestBase {
     );
   }
 
-  protected static Stream<Arguments> invalidModels() {
+  static Stream<Arguments> invalidModels() {
     return Stream.of(
       // for-each with expression of non-iterable, non-generic type
       arg("""
-        import java.lang.Boolean;
-        component InvalidComp1 {
-          Boolean v = true;
-          automaton {
-            initial state S {
-              -> / {
+          import java.lang.Boolean;
+          component InvalidComp1 {
+            Boolean v = true;
+            automaton {
+              initial state S;
+              S -> S / {
                 for (Boolean i : v) { }
               }
             }
           }
-        }
-        """, MCError.FOR_EACH_EXPR_NOT_ITERABLE
+          """,
+        FOR_EACH_EXPR_NOT_ITERABLE
       ),
       // for-each with expression of non-iterable, generic type
       arg("""
-        import java.lang.Boolean;
-        import java.util.Optional;
-        component InvalidComp2 {
-          Optional<Boolean> v = Optional.of(true);
-          automaton {
-            initial state S {
-              -> / {
+          import java.lang.Boolean;
+          import java.util.Optional;
+          component InvalidComp2 {
+            Optional<Boolean> v = Optional.of(true);
+            automaton {
+              initial state S;
+              S -> S / {
                 for (Boolean i : v) { }
               }
             }
           }
-        }
-        """, MCError.FOR_EACH_EXPR_NOT_ITERABLE
+          """,
+        FOR_EACH_EXPR_NOT_ITERABLE
       ),
       // for-each with mismatching types
       arg("""
-        import java.lang.Boolean;
-        import java.lang.Integer;
-        import java.util.Collection;
-        import java.util.Collections;
-        component InvalidComp3 {
-          Collection<Boolean> v = Collections.emptyList();
-          automaton {
-            initial state S {
-              -> / {
+          import java.lang.Boolean;
+          import java.lang.Integer;
+          import java.util.Collection;
+          import java.util.Collections;
+          component InvalidComp3 {
+            Collection<Boolean> v = [ ];
+            automaton {
+              initial state S;
+              S -> S / {
                 for (Integer i : v) { }
               }
             }
           }
-        }
-        """, MCError.FOR_EACH_TYPE_MISMATCH
+          """,
+        FOR_EACH_TYPE_MISMATCH
       ),
       // for-each with mismatching super- and subtype
       arg("""
-        import java.lang.Integer;
-        import java.lang.Number;
-        import java.util.Collection;
-        import java.util.Collections;
-        component InvalidComp4 {
-          Collection<Number> v = Collections.emptyList();
-          automaton {
-            initial state S {
-              -> / {
+          import java.lang.Integer;
+          import java.lang.Number;
+          import java.util.Collection;
+          import java.util.Collections;
+          component InvalidComp4 {
+            Collection<Number> v = [ ];
+            automaton {
+              initial state S;
+              S -> S / {
                 for (Integer i : v) { }
               }
             }
           }
-        }
-        """, MCError.FOR_EACH_TYPE_MISMATCH
+          """,
+        FOR_EACH_TYPE_MISMATCH
       )
     );
   }
