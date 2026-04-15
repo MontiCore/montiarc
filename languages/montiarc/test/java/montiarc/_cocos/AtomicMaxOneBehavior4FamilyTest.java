5b1cfcda568356d69a0824ff6226ee4b5208fa15
David Schmalzing <schmalzing@se-rwth.de>
2026-04-16 01:22:15 +0200

    Minor layouting

--- languages/montiarc/test/java/montiarc/_cocos/AtomicMaxOneBehavior4FamilyTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/AtomicMaxOneBehavior4FamilyTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junitpioneer.jupiter.params.DisableIfDisplayName;
 import variablearc._cocos.AtomicMaxOneBehavior4Family;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
 import static montiarc.util.ArcError.MULTIPLE_BEHAVIOR;
@@ -26,7 +25,7 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
   @ParameterizedTest
   @MethodSource("validModels")
   @MethodSource("validModelsWithVariability")
-  void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -50,7 +49,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
     "InvalidCompWithVariability6",
     "InvalidCompWithVariability8"
   })
-  void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -69,6 +69,7 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
 
   protected static Stream<Arguments> validModelsWithVariability() {
     return Stream.of(
+      // atomic component with an automaton in the if-branch and an automaton in the else-branch
       arg(
         """
           component ValidCompWithVariability1 {
@@ -78,6 +79,7 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
           }
           """
       ),
+      // atomic component with two variant automata, mutually exclusive via constraint(f1 ^ f2)
       arg(
         """
           component ValidCompWithVariability2 {
@@ -88,6 +90,7 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
           }
           """
       ),
+      // inner component with two variant automata, mutually exclusive via constraint(f1 ^ f2)
       arg(
         """
           component ValidCompWithVariability3 {
@@ -100,6 +103,7 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
           }
           """
       ),
+      // atomic component with three variant automata, pairwise mutually exclusive via constraints
       arg(
         """
           component ValidCompWithVariability4 {
@@ -126,7 +130,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f1) { automaton { } }
             varif(f2) { automaton { } }
           }
-          """, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR
       ),
       // atomic component with two ajava blocks (configuration f1 && f2)
       arg(
@@ -135,7 +140,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             feature f1, f2;
             varif(f1) { compute { } }
             varif(f2) { compute { } }
-          }""", MULTIPLE_BEHAVIOR
+          }""",
+        MULTIPLE_BEHAVIOR
       ),
       // atomic component with automaton and ajava (configuration f1 && f2)
       arg(
@@ -145,7 +151,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f1) { automaton { } }
             varif(f2) { compute { } }
           }
-          """, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR
       ),
       // inner component with two automata (configuration f1 && f2)
       arg(
@@ -157,7 +164,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
               varif(f2) { automaton { } }
             }
           }
-          """, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR
       ),
       // atomic component with three automata (configuration f1 && f2 && f3)
       arg(
@@ -168,7 +176,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f2) { automaton { } }
             varif(f3) { automaton { } }
           }
-          """, MULTIPLE_BEHAVIOR, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR, MULTIPLE_BEHAVIOR
       ),
       // atomic component with three automata (configuration f1 && f2)
       arg(
@@ -178,7 +187,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f1) { automaton { } automaton { } }
             varif(f2) { automaton { } }
           }
-          """, MULTIPLE_BEHAVIOR, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR, MULTIPLE_BEHAVIOR
       ),
       // atomic component with two automata (configuration f1 && !f2)
       arg(
@@ -189,9 +199,10 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f2) { automaton { } }
             constraint(f1 ^ f2);
           }
-          """, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR
       ),
-      // atomic component with two automata (configuration f1 ^ f2)
+      // atomic component with two automata in each of the two variants allowed by constraint(f1 ^ f2)
       arg(
         """
           component InvalidCompWithVariability8 {
@@ -200,7 +211,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f2) { automaton { } automaton { } }
             constraint(f1 ^ f2);
           }
-          """, MULTIPLE_BEHAVIOR, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR, MULTIPLE_BEHAVIOR
       ),
       // atomic component with two automata (configuration f2 && !f1)
       arg(
@@ -211,7 +223,8 @@ class AtomicMaxOneBehavior4FamilyTest extends AtomicMaxOneBehaviorTest {
             varif(f2) { automaton { } automaton { } }
             constraint(f1 ^ f2);
           }
-          """, MULTIPLE_BEHAVIOR
+          """,
+        MULTIPLE_BEHAVIOR
       )
     );
   }
