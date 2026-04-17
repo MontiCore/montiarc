dde59854709396856e554553255e9efaa2df0add
David Pascal Schmalzing <schmalzing@se.rwth-aachen.de>
2026-04-17 14:39:48 +0200

    Rework behavior in decomposed tests

--- languages/montiarc/test/java/montiarc/_cocos/BehaviorInDecomposedTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/BehaviorInDecomposedTest.java
@@ -2,87 +2,138 @@
 package montiarc._cocos;
 
 import arcbasis._cocos.BehaviorInDecomposed;
+import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcError;
 import montiarc.util.Error;
-import org.assertj.core.api.Assertions;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.DECOMPOSED_COMPONENT_WITH_BEHAVIOR;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * The class under test is {@link arcbasis._cocos.BehaviorInDecomposed}.
+ * The class under test is {@link BehaviorInDecomposed}.
  */
-public class BehaviorInDecomposedTest extends MontiArcTestBase {
+class BehaviorInDecomposedTest extends MontiArcTestBase {
 
   @BeforeEach
   protected void setUpComponents() {
-    MontiArcTestBase.compile("package a.b; component A {}");
+    compile("package a.b; component A {}");
   }
 
   @ParameterizedTest
   @MethodSource("validModels")
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
+    Preconditions.checkNotNull(model);
+
+    // Given
     ASTMACompilationUnit ast = MontiArcTestBase.compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new BehaviorInDecomposed());
 
+    // When
     checker.checkAll(ast);
 
+    // Then
     assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error[] expectedErrors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error[] expectedErrors) {
+    // Given
     ASTMACompilationUnit ast = MontiArcTestBase.compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new BehaviorInDecomposed());
 
+    // When
     checker.checkAll(ast);
 
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
-    Assertions.assertThat(getLoggedErrorCodes())
+    // Then
+    assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(expectedErrors));
   }
 
   protected static Stream<Arguments> validModels() {
     return Stream.of(
-      Arguments.of("component C1 { a.b.A a; }"),
-      Arguments.of("component C2 { component A {} compute {} }"),
-      Arguments.of("component C3 { automaton {} }"),
-      Arguments.of("component C4 {}")
+      // decomposed component (subcomponent instance) without its own behavior
+      arg("""
+        component ValidComp1 {
+          a.b.A a;
+        }
+        """
+      ),
+      // atomic component with an uninstantiated nested component type and its own behavior
+      arg("""
+        component ValidComp2 {
+          component A { }
+          compute {}
+        }
+        """
+      ),
+      // atomic component with an automaton
+      arg("""
+        component ValidComp3 {
+          automaton { }
+        }
+        """
+      ),
+      // atomic component without behavior
+      arg("component ValidComp4 { }")
     );
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      Arguments.of(
-        "component C5 { a.b.A a; compute {} }",
-        new Error[]{ ArcError.DECOMPOSED_COMPONENT_WITH_BEHAVIOR }
+      // decomposed component with ajava behavior
+      arg("""
+          component InvalidComp1 {
+            a.b.A a;
+            compute {}
+          }""",
+        DECOMPOSED_COMPONENT_WITH_BEHAVIOR
       ),
-      Arguments.of(
-        "component C6 { a.b.A a; automaton { initial state S; } }",
-        new Error[]{ ArcError.DECOMPOSED_COMPONENT_WITH_BEHAVIOR }
+      // decomposed component with automaton behavior
+      arg("""
+          component InvalidComp2 {
+            a.b.A a; automaton {
+              initial state S;
+            }
+          }
+          """,
+        DECOMPOSED_COMPONENT_WITH_BEHAVIOR
       ),
-      Arguments.of(
-        "component C7 { a.b.A a; compute {} automaton { initial state S; } }",
-        new Error[]{ ArcError.DECOMPOSED_COMPONENT_WITH_BEHAVIOR }
+      // decomposed component with ajava and automaton behavior
+      arg("""
+          component InvalidComp3 {
+            a.b.A a;
+            compute { }
+            automaton {
+              initial state S;
+            }
+          }
+          """,
+        DECOMPOSED_COMPONENT_WITH_BEHAVIOR
       ),
-      Arguments.of(
-        "component C8 { component A { a.b.A a; compute {} } }",
-        new Error[]{ ArcError.DECOMPOSED_COMPONENT_WITH_BEHAVIOR }
+      // nested decomposed component with ajava behavior
+      arg("""
+          component InvalidComp4 {
+            component A {
+              a.b.A a;
+              compute { }
+            }
+          }""",
+        DECOMPOSED_COMPONENT_WITH_BEHAVIOR
       )
     );
   }
