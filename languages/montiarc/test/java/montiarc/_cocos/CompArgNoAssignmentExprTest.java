ffd2e585e39bae5874726c596863d824ee8ecb35
David Pascal Schmalzing <schmalzing@se.rwth-aachen.de>
2026-04-17 15:11:18 +0200

    Rework comp arg no assignment expr tests

--- languages/montiarc/test/java/montiarc/_cocos/CompArgNoAssignmentExprTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/CompArgNoAssignmentExprTest.java
@@ -8,7 +8,6 @@ import montiarc.MontiArcMill;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.trafo.MontiArcTrafos;
-import montiarc.util.ArcError;
 import montiarc.util.Error;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,9 +19,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.INVALID_CONTEXT_ASSIGNMENT;
+import static montiarc.util.ArcError.INVALID_CONTEXT_DEC_PREFIX;
+import static montiarc.util.ArcError.INVALID_CONTEXT_DEC_SUFFIX;
+import static montiarc.util.ArcError.INVALID_CONTEXT_INC_PREFIX;
+import static montiarc.util.ArcError.INVALID_CONTEXT_INC_SUFFIX;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CompArgNoAssignmentExprTest extends MontiArcTestBase {
+/**
+ * The class under test is {@link CompArgNoAssignmentExpr}.
+ */
+class CompArgNoAssignmentExprTest extends MontiArcTestBase {
 
   public static ASTMACompilationUnit compile(@NotNull String model) {
     Preconditions.checkNotNull(model);
@@ -45,14 +52,20 @@ public class CompArgNoAssignmentExprTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
-    "component Comp1 { a.b.A a(); }",
-    "component Comp2 { a.b.B b(1); }",
-    "component Comp3 { a.b.B b(p = 1); }",
-    "component Comp4 { a.b.C c(1, 2); }",
-    "component Comp5 { a.b.C c(1, p2 = 2); }",
-    "component Comp6 { a.b.C c(p1 = 1, p2 = 2); }",
+    // subcomponent instantiation without arguments
+    "component ValidComp1 { a.b.A a(); }",
+    // single positional argument
+    "component ValidComp2 { a.b.B b(1); }",
+    // single keyed argument
+    "component ValidComp3 { a.b.B b(p = 1); }",
+    // two positional arguments
+    "component ValidComp4 { a.b.C c(1, 2); }",
+    // one positional and one keyed argument
+    "component ValidComp5 { a.b.C c(1, p2 = 2); }",
+    // two keyed arguments
+    "component ValidComp6 { a.b.C c(p1 = 1, p2 = 2); }",
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -65,12 +78,13 @@ public class CompArgNoAssignmentExprTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -84,66 +98,193 @@ public class CompArgNoAssignmentExprTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      arg("component Comp1 { a.b.B b(p = p = 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp2 { a.b.B b(p = p += 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp3 { a.b.B b(p = p -= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp4 { a.b.B b(p = p *= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp5 { a.b.B b(p = p /= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp6 { a.b.B b(p = p /= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp7 { a.b.B b(p = p &= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp8 { a.b.B b(p = p |= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp9 { a.b.B b(p = p ^= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp10 { a.b.B b(p = p >>= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp11 { a.b.B b(p = p <<= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp12 { a.b.B b(p = p %= 1); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp13 { a.b.C c(p1 = 1, p2 = p2 = 2); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp14 { a.b.C c(p1 = p1 = 1, p2 = 2); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp15 { a.b.C c(p1 = p1 = 1, p2 = p2 = 2); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT,
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp16 { a.b.C c(p1 = p1 = 1, p2 = p2 = 2); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT,
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp17 { a.b.C c(p1 = p2 = 1, p1 = p2 = 2); }",
-        ArcError.INVALID_CONTEXT_ASSIGNMENT,
-        ArcError.INVALID_CONTEXT_ASSIGNMENT),
-      arg("component Comp18 { a.b.B b(++p); }",
-        ArcError.INVALID_CONTEXT_INC_PREFIX),
-      arg("component Comp19 { a.b.B b(--p); }",
-        ArcError.INVALID_CONTEXT_DEC_PREFIX),
-      arg("component Comp20 { a.b.B b(p++); }",
-        ArcError.INVALID_CONTEXT_INC_SUFFIX),
-      arg("component Comp21 { a.b.B b(p--); }",
-        ArcError.INVALID_CONTEXT_DEC_SUFFIX),
-      arg("component Comp22 { a.b.B b(p = ++p); }",
-        ArcError.INVALID_CONTEXT_INC_PREFIX),
-      arg("component Comp23 { a.b.B b(p = --p); }",
-        ArcError.INVALID_CONTEXT_DEC_PREFIX),
-      arg("component Comp24 { a.b.B b(p = p++); }",
-        ArcError.INVALID_CONTEXT_INC_SUFFIX),
-      arg("component Comp25 { a.b.B b(p = p--); }",
-        ArcError.INVALID_CONTEXT_DEC_SUFFIX)
+      // nested assignment (=) as a component argument value
+      arg("""
+          component InvalidComp1 {
+            a.b.B b(p = p = 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested addition-assignment (+=) as a component argument value
+      arg("""
+          component InvalidComp2 {
+            a.b.B b(p = p += 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested subtraction-assignment (-=) as a component argument value
+      arg("""
+          component InvalidComp3 {
+            a.b.B b(p = p -= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested multiplication-assignment (*=) as a component argument value
+      arg("""
+          component InvalidComp4 {
+            a.b.B b(p = p *= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested division-assignment (/=) as a component argument value
+      arg("""
+          component InvalidComp5 {
+            a.b.B b(p = p /= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested unsigned-right-shift-assignment (>>>=) as a component argument value
+      arg("""
+          component InvalidComp6 {
+            a.b.B b(p = p >>>= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested and-assignment (&=) as a component argument value
+      arg("""
+          component InvalidComp7 {
+            a.b.B b(p = p &= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested or-assignment (|=) as a component argument value
+      arg("""
+          component InvalidComp8 {
+            a.b.B b(p = p |= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested xor-assignment (^=) as a component argument value
+      arg("""
+          component InvalidComp9 {
+            a.b.B b(p = p ^= 1);
+          }""",
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested right-shift-assignment (>>=) as a component argument value
+      arg("""
+          component InvalidComp10 {
+            a.b.B b(p = p >>= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested left-shift-assignment (<<=) as a component argument value
+      arg("""
+          component InvalidComp11 {
+            a.b.B b(p = p <<= 1);
+          }""",
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // nested modulo-assignment (%=) as a component argument value
+      arg("""
+          component InvalidComp12 {
+            a.b.B b(p = p %= 1);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // one plain keyed argument, one keyed argument with a nested assignment
+      arg("""
+          component InvalidComp13 {
+            a.b.C c(p1 = 1, p2 = p2 = 2);
+          }
+          """,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // one keyed argument with a nested assignment, one plain keyed argument
+      arg("""
+          component InvalidComp14 {
+            a.b.C c(p1 = p1 = 1, p2 = 2);
+          }""",
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // two keyed arguments, each with a nested assignment to the same parameter
+      arg("""
+          component InvalidComp15 {
+            a.b.C c(p1 = p1 = 1, p2 = p2 = 2);
+          }""",
+        INVALID_CONTEXT_ASSIGNMENT,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // two keyed arguments, each with a nested assignment to a different parameter
+      arg("""
+          component InvalidComp16 {
+            a.b.C c(p1 = p2 = 1, p1 = p2 = 2);
+          }""",
+        INVALID_CONTEXT_ASSIGNMENT,
+        INVALID_CONTEXT_ASSIGNMENT
+      ),
+      // prefix increment expression as a positional component argument
+      arg("""
+          component InvalidComp17 {
+            a.b.B b(++p);
+          }""",
+        INVALID_CONTEXT_INC_PREFIX
+      ),
+      // prefix decrement expression as a positional component argument
+      arg("""
+          component InvalidComp18 {
+            a.b.B b(--p);
+          }""",
+        INVALID_CONTEXT_DEC_PREFIX
+      ),
+      // suffix increment expression as a positional component argument
+      arg("""
+          component InvalidComp19 {
+            a.b.B b(p++);
+          }""",
+        INVALID_CONTEXT_INC_SUFFIX
+      ),
+      // suffix decrement expression as a positional component argument
+      arg("""
+          component InvalidComp20 {
+            a.b.B b(p--);
+          }""",
+        INVALID_CONTEXT_DEC_SUFFIX
+      ),
+      // keyed argument with a nested prefix increment expression
+      arg("""
+          component InvalidComp21 {
+            a.b.B b(p = ++p);
+          }""",
+        INVALID_CONTEXT_INC_PREFIX
+      ),
+      // keyed argument with a nested prefix decrement expression
+      arg("""
+          component InvalidComp22 {
+            a.b.B b(p = --p);
+          }""",
+        INVALID_CONTEXT_DEC_PREFIX
+      ),
+      // keyed argument with a nested suffix increment expression
+      arg("""
+          component InvalidComp23 {
+            a.b.B b(p = p++);
+          }""",
+        INVALID_CONTEXT_INC_SUFFIX
+      ),
+      // keyed argument with a nested suffix decrement expression
+      arg("""
+          component InvalidComp24 {
+            a.b.B b(p = p--);
+          }""",
+        INVALID_CONTEXT_DEC_SUFFIX
+      )
     );
   }
 }
