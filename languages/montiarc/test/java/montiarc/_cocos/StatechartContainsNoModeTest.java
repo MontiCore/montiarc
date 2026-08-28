f46cb22533a3db40128423b65bd55a2fee1c9fae
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 21:40:56 +0200

    Rework statechart contains no mode tests

--- languages/montiarc/test/java/montiarc/_cocos/StatechartContainsNoModeTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/StatechartContainsNoModeTest.java
@@ -1,75 +1,57 @@
 /* (c) https://github.com/MontiCore/monticore */
 package montiarc._cocos;
 
-import arcautomaton._ast.ASTArcStatechart;
 import com.google.common.base.Preconditions;
-import de.se_rwth.commons.logging.Log;
 import modes._cocos.StatechartContainsNoMode;
-import montiarc.MontiArcMill;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc._symboltable.IMontiArcScope;
 import montiarc.util.Error;
-import montiarc.util.ModesError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ModesError.STATECHART_CONTAINS_MODE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link StatechartContainsNoMode}
+ * The class under test is {@link StatechartContainsNoMode}.
  */
-public class StatechartContainsNoModeTest extends MontiArcTestBase {
-
-  protected static Stream<Arguments> numberOfModesAndStatesWithErrorProvider() {
-    return Stream.of(
-      Arguments.of(0, 0, new ModesError[]{}),
-      Arguments.of(1, 0, new ModesError[]{}),
-      Arguments.of(2, 0, new ModesError[]{}),
-      Arguments.of(3, 0, new ModesError[]{}),
-      Arguments.of(0, 1, new ModesError[]{ModesError.STATECHART_CONTAINS_MODE}),
-      Arguments.of(1, 1, new ModesError[]{ModesError.STATECHART_CONTAINS_MODE}),
-      Arguments.of(2, 1, new ModesError[]{ModesError.STATECHART_CONTAINS_MODE}),
-      Arguments.of(3, 1, new ModesError[]{ModesError.STATECHART_CONTAINS_MODE}),
-      Arguments.of(0, 2, new ModesError[]{ModesError.STATECHART_CONTAINS_MODE}),
-      Arguments.of(1, 2, new ModesError[]{ModesError.STATECHART_CONTAINS_MODE})
-    );
-  }
+class StatechartContainsNoModeTest extends MontiArcTestBase {
 
   @ParameterizedTest
-  @MethodSource("numberOfModesAndStatesWithErrorProvider")
-  public void testCocoWithNModeAutomata(int numberOfStates, int numberOfModes, @NotNull ModesError... errors) {
-    Preconditions.checkArgument(numberOfModes >= 0);
-    Preconditions.checkArgument(numberOfStates >= 0);
-    Preconditions.checkNotNull(errors);
+  @ValueSource(strings = {
+    // no states, no modes
+    "component ValidComp1 { automaton { } }",
+    // one state, no modes
+    "component ValidComp2 { automaton { state s0; } }",
+    // two states, no modes
+    "component ValidComp3 { automaton { state s0; state s1; } }",
+    // three states, no modes
+    "component ValidComp4 { automaton { state s0; state s1; state s2; } }"
+  })
+  void shouldNotReportError(@NotNull String model) {
+    Preconditions.checkNotNull(model);
 
     // Given
-    ASTArcStatechart statechart = MontiArcMill.arcStatechartBuilder().build();
-    statechart.setSpannedScope(MontiArcMill.scope());
-    for (int i = 0; i < numberOfModes; i++) {
-      ((IMontiArcScope) statechart.getSpannedScope()).add(MontiArcMill.arcModeSymbolBuilder().setName("m" + i).build());
-    }
-    for (int i = 0; i < numberOfStates; i++) {
-      statechart.getSpannedScope().add(MontiArcMill.sCStateSymbolBuilder().setName("s" + i).build());
-    }
+    ASTMACompilationUnit ast = compile(model);
+
+    MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
+    checker.addCoCo(new StatechartContainsNoMode());
 
     // When
-    StatechartContainsNoMode coco = new StatechartContainsNoMode();
-    coco.check(statechart);
+    checker.checkAll(ast);
 
     // Then
-    assertThat(getLoggedErrorCodes())
-      .containsExactlyInAnyOrder(getErrorCodes(errors));
+    assertThat(getLoggedErrorCodes()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model, @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -83,35 +65,33 @@ public class StatechartContainsNoModeTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      // automaton with a mode
-      arg("component Comp1 { " +
-          "automaton { mode m1 { } } " +
-          "}",
-        ModesError.STATECHART_CONTAINS_MODE),
-      // automaton with multiple states and modes
-      arg("component Comp2 { " +
-          "automaton { " +
-          "state s1; " +
-          "mode m1 { }" +
-          "state s2; " +
-          "} " +
-          "}",
-        ModesError.STATECHART_CONTAINS_MODE),
-      // inner with two automata where one has a mode
-      arg("component Comp3 { " +
-          "component Inner { " +
-          "automaton { state s1; } " +
-          "automaton { state s1; mode m1 { } } " +
-          "} " +
-          "}",
-        ModesError.STATECHART_CONTAINS_MODE)
+      // no states, one mode
+      arg("component InvalidComp1 { automaton { mode m1 { } } }",
+        STATECHART_CONTAINS_MODE),
+      // two states, one mode (mode declared between states)
+      arg("component InvalidComp2 { automaton { state s1; mode m1 { } state s2; } }",
+        STATECHART_CONTAINS_MODE),
+      // one state, one mode
+      arg("component InvalidComp3 { automaton { state s0; mode m0 { } } }",
+        STATECHART_CONTAINS_MODE),
+      // three states, one mode
+      arg("component InvalidComp4 { automaton { state s0; state s1; state s2; mode m0 { } } }",
+        STATECHART_CONTAINS_MODE),
+      // no states, two modes
+      arg("component InvalidComp5 { automaton { mode m0 { } mode m1 { } } }",
+        STATECHART_CONTAINS_MODE),
+      // one state, two modes
+      arg("component InvalidComp6 { automaton { state s0; mode m0 { } mode m1 { } } }",
+        STATECHART_CONTAINS_MODE),
+      // nested component type with two automata, one with a mode
+      arg("component InvalidComp7 { component Inner { automaton { state s1; } automaton { state s1; mode m1 { } } } }",
+        STATECHART_CONTAINS_MODE)
     );
   }
 }
