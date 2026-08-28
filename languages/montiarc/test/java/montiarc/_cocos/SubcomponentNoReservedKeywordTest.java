027033052f3b60e227622bc925ce3b8cef810aed
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 20:26:02 +0200

    Rework subcomponent no reserved keyword tests

--- languages/montiarc/test/java/montiarc/_cocos/SubcomponentNoReservedKeywordTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/SubcomponentNoReservedKeywordTest.java
@@ -6,7 +6,6 @@ import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcError;
 import montiarc.util.Error;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,29 +13,37 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.RESTRICTED_IDENTIFIER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The class under test is {@link SubcomponentNoReservedKeyword}.
  */
-public class SubcomponentNoReservedKeywordTest extends MontiArcTestBase {
+class SubcomponentNoReservedKeywordTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
-    "package key; component Comp1 { }",
-    "component Comp2 { component I { } I sub; }",
-    "component Comp3(int key) {  }",
-    "component Comp4<key> { }",
-    "component Comp5 { int key = 1; }",
-    "component Comp6 { port in int key; }",
-    "component Comp7 { automaton { state key; } }",
-    "component Comp8 { component key { } }",
+    // reserved keyword used as a package name
+    "package key; component ValidComp1 { }",
+    // regular subcomponent declaration, no reserved keyword involved
+    "component ValidComp2 { component I { } I sub; }",
+    // reserved keyword used as a parameter name
+    "component ValidComp3(int key) {  }",
+    // reserved keyword used as a generic type parameter name
+    "component ValidComp4<key> { }",
+    // reserved keyword used as a field name
+    "component ValidComp5 { int key = 1; }",
+    // reserved keyword used as a port name
+    "component ValidComp6 { port in int key; }",
+    // reserved keyword used as an automaton state name
+    "component ValidComp7 { automaton { state key; } }",
+    // reserved keyword used as a nested component type name
+    "component ValidComp8 { component key { } }",
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -49,12 +56,13 @@ public class SubcomponentNoReservedKeywordTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -68,23 +76,26 @@ public class SubcomponentNoReservedKeywordTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(errors.length);
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      arg("component Comp1 { component Inner { } Inner key; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp2 { component Inner { } Inner word; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp3 { component Inner { } Inner key, key; }",
-        ArcError.RESTRICTED_IDENTIFIER,
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp4 { component Inner { } Inner key; Inner key; }",
-        ArcError.RESTRICTED_IDENTIFIER,
-        ArcError.RESTRICTED_IDENTIFIER)
+      // subcomponent instance named after the first reserved keyword
+      arg("component InvalidComp1 { component Inner { } Inner key; }",
+        RESTRICTED_IDENTIFIER),
+      // subcomponent instance named after the second reserved keyword
+      arg("component InvalidComp2 { component Inner { } Inner word; }",
+        RESTRICTED_IDENTIFIER),
+      // two subcomponent instances declared together, both named after the same reserved keyword
+      arg("component InvalidComp3 { component Inner { } Inner key, key; }",
+        RESTRICTED_IDENTIFIER,
+        RESTRICTED_IDENTIFIER),
+      // two subcomponent instances declared separately, both named after the same reserved keyword
+      arg("component InvalidComp4 { component Inner { } Inner key; Inner key; }",
+        RESTRICTED_IDENTIFIER,
+        RESTRICTED_IDENTIFIER)
     );
   }
 }
