32e1fb37937f8204e4ae0571ae130479f46ebdbd
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 19:30:15 +0200

    Rework no code block in mode transitions tests

--- languages/montiarc/test/java/montiarc/_cocos/NoCodeBlockInModeTransitionsTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/NoCodeBlockInModeTransitionsTest.java
@@ -1,74 +1,119 @@
 /* (c) https://github.com/MontiCore/monticore */
 package montiarc._cocos;
 
+import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
 import modes._cocos.NoCodeBlockInModeTransitions;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.Error;
-import montiarc.util.ModesError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ModesError.MODE_AUTOMATON_TRANSITION_CONTAINS_ACTION;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link NoCodeBlockInModeTransitions}
+ * The class under test is {@link NoCodeBlockInModeTransitions}.
  */
-public class NoCodeBlockInModeTransitionsTest extends MontiArcTestBase {
+class NoCodeBlockInModeTransitionsTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @MethodSource("validModels")
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
+    Preconditions.checkNotNull(model);
+
+    // Given
     ASTMACompilationUnit ast = compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new NoCodeBlockInModeTransitions());
 
+    // When
     checker.checkAll(ast);
 
+    // Then
     assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error[] expectedErrors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
+    Preconditions.checkNotNull(model);
+    Preconditions.checkNotNull(errors);
+
+    // Given
     ASTMACompilationUnit ast = compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new NoCodeBlockInModeTransitions());
 
+    // When
     checker.checkAll(ast);
 
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
-    assertThat(getLoggedErrorCodes()).containsExactlyInAnyOrder(getErrorCodes(expectedErrors));
+    // Then
+    assertThat(getLoggedErrorCodes())
+      .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> validModels() {
     return Stream.of(
-      Arguments.of(
-        "component Comp1 { mode automaton { initial mode A { } mode B { } A -> B; } }"
+      // mode transition without action block
+      arg("""
+        component ValidComp1 {
+          mode automaton {
+            initial mode A { }
+            mode B { }
+            A -> B;
+          }
+        }
+        """
       ),
-      Arguments.of(
-        "component Comp2 { mode automaton { initial mode Init { } mode Ready { } Init -> Ready; } }"
+      // mode transition without action block
+      arg("""
+        component ValidComp2 {
+          mode automaton {
+            initial mode Init { }
+            mode Ready { }
+            Init -> Ready;
+          }
+        }
+        """
       )
     );
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      Arguments.of(
-        "component Comp3 { port out int x; mode automaton { initial mode A { } mode B { } A -> B / { x = 1; } } }",
-        new Error[]{ModesError.MODE_AUTOMATON_TRANSITION_CONTAINS_ACTION}
+      // mode transition with a non-empty action block
+      arg("""
+        component InvalidComp1 {
+          port out int x;
+          mode automaton {
+            initial mode A { }
+            mode B { }
+            A -> B / { x = 1; }
+          }
+        }
+        """,
+        MODE_AUTOMATON_TRANSITION_CONTAINS_ACTION
       ),
-      Arguments.of(
-        "component Comp4 { mode automaton { initial mode A { } mode B { } A -> B / { } } }",
-        new Error[]{ModesError.MODE_AUTOMATON_TRANSITION_CONTAINS_ACTION}
+      // mode transition with an empty action block
+      arg("""
+        component InvalidComp2 {
+          mode automaton {
+            initial mode A { }
+            mode B { }
+            A -> B / { }
+          }
+        }
+        """,
+        MODE_AUTOMATON_TRANSITION_CONTAINS_ACTION
       )
     );
   }
