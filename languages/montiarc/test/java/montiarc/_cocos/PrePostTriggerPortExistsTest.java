39434bfb88fc05ac54d179e04b72d4e400254552
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 20:42:11 +0200

    Rework pre post trigger port exists tests

--- languages/montiarc/test/java/montiarc/_cocos/PrePostTriggerPortExistsTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/PrePostTriggerPortExistsTest.java
@@ -7,49 +7,52 @@ import de.se_rwth.commons.logging.Log;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.Error;
-import montiarc.util.PrePostError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.PrePostError.TRIGGER_MISSING_PORT;
+import static montiarc.util.PrePostError.TRIGGER_NOT_AN_IN_PORT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class PrePostTriggerPortExistsTest extends MontiArcTestBase {
+/**
+ * The class under test is {@link TriggerPortExists}.
+ */
+class PrePostTriggerPortExistsTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
     // simple port
     """
-      component SimplePort {
+      component ValidComp1 {
         port in int id;
         trigger: id;
         post: true;
       }
-   """,
-    //generic port
-    """ 
-      component GenericPort<T> {
+      """,
+    // generic port
+    """
+      component ValidComp2<T> {
         port in T id;
         trigger: id;
         post: true;
       }
-    """,
-    // We check multiple ports in other cocos. Here we only emit a debug message
-    """ 
-      component MultiplePorts {
+      """,
+    // we check multiple ports in other cocos, here we only emit a debug message
+    """
+      component ValidComp3 {
         port in int id;
         port in int id;
         trigger: id;
         post: true;
       }
-    """,
+      """,
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -62,12 +65,13 @@ public class PrePostTriggerPortExistsTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -81,50 +85,51 @@ public class PrePostTriggerPortExistsTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      arg(
-        """ 
-          component NoPort {
-            trigger: id;
-            post: true;
-          }
+      // trigger references an identifier that isn't declared as a port
+      arg("""
+        component InvalidComp1 {
+          trigger: id;
+          post: true;
+        }
         """,
-        PrePostError.TRIGGER_MISSING_PORT),
-
-      arg(
-        """ 
-          component WrongName {
-            port in int Id;
-            trigger: id;
-            post: true;
-          }
+        TRIGGER_MISSING_PORT
+      ),
+      // trigger references a port name with a different case than the declared port
+      arg("""
+        component InvalidComp2 {
+          port in int Id;
+          trigger: id;
+          post: true;
+        }
         """,
-        PrePostError.TRIGGER_MISSING_PORT),
-      arg(
-        """ 
-          component OutPort {
-            port out int id;
-            trigger: id;
-            post: true;
-          }
+        TRIGGER_MISSING_PORT
+      ),
+      // trigger references an output port instead of an input port
+      arg("""
+        component InvalidComp3 {
+          port out int id;
+          trigger: id;
+          post: true;
+        }
         """,
-        PrePostError.TRIGGER_NOT_AN_IN_PORT),
-      arg(
-        """ 
-          component PortIsAttribute {
-            int id = 0;
-            trigger: id;
-            post: true;
-          }
+        TRIGGER_NOT_AN_IN_PORT
+      ),
+      // trigger references a field instead of a port
+      arg("""
+        component InvalidComp4 {
+          int id = 0;
+          trigger: id;
+          post: true;
+        }
         """,
-        PrePostError.TRIGGER_MISSING_PORT)
-
+        TRIGGER_MISSING_PORT
+      )
     );
   }
 }
