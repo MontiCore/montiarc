69da56c07db11fbfe8ad422b6872c022e9385813
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 20:43:39 +0200

    Rework max one mode automaton tests

--- languages/montiarc/test/java/montiarc/_cocos/MaxOneModeAutomatonTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/MaxOneModeAutomatonTest.java
@@ -7,7 +7,6 @@ import modes._cocos.MaxOneModeAutomaton;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.Error;
-import montiarc.util.ModesError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,65 +14,39 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ModesError.MULTIPLE_MODE_AUTOMATA;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The class under test is {@link MaxOneModeAutomaton}.
  */
-public class MaxOneModeAutomatonTest extends MontiArcTestBase {
+class MaxOneModeAutomatonTest extends MontiArcTestBase {
 
   @BeforeEach
-  public void setUpComponent() {
+  protected void setUpComponent() {
     compile("package a.b; component A { }");
   }
 
   @ParameterizedTest
   @ValueSource(strings = {
-    // decomposed no mode
-    "component Comp1 { " +
-      "a.b.A a;" +
-      "}",
-    // decomposed with mode
-    "component Comp2 { " +
-      "a.b.A a;" +
-      "mode automaton { } " +
-      "}",
-    // inner with mode
-    "component Comp3 { " +
-      "component Inner { " +
-      "a.b.A a;" +
-      "mode automaton { } " +
-      "} " +
-      "}",
-    // two inner with modes
-    "component Comp4 { " +
-      "component Inner1 { " +
-      "a.b.A a;" +
-      "mode automaton { } " +
-      "} " +
-      "component Inner2 { " +
-      "a.b.A a;" +
-      "mode automaton { } " +
-      "} " +
-      "}",
-    // decomposed component with automaton and mode
-    "component Comp5 { " +
-      "a.b.A a;" +
-      "mode automaton { } " +
-      "automaton { } " +
-      "}",
-    // atomic component with no mode
-    "component Comp6 { }",
-    // atomic component with two modes
-    "component Comp7 { " +
-      "mode automaton { } " +
-      "mode automaton { } " +
-      "}",
+    // decomposed component without a mode automaton
+    "component ValidComp1 { a.b.A a; }",
+    // decomposed component with a mode automaton
+    "component ValidComp2 { a.b.A a; mode automaton { } }",
+    // nested decomposed component type with a mode automaton
+    "component ValidComp3 { component Inner { a.b.A a; mode automaton { } } }",
+    // two nested decomposed component types, each with a mode automaton
+    "component ValidComp4 { component Inner1 { a.b.A a; mode automaton { } } component Inner2 { a.b.A a; mode automaton { } } }",
+    // decomposed component with both a mode automaton and a regular automaton
+    "component ValidComp5 { a.b.A a; mode automaton { } automaton { } }",
+    // atomic component without a mode automaton
+    "component ValidComp6 { }",
+    // atomic component with two mode automata (this CoCo only checks decomposed components)
+    "component ValidComp7 { mode automaton { } mode automaton { } }",
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -86,12 +59,13 @@ public class MaxOneModeAutomatonTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -105,29 +79,18 @@ public class MaxOneModeAutomatonTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      // decomposed with two automata
-      arg("component Comp1 { " +
-          "a.b.A a;" +
-          "mode automaton { } " +
-          "mode automaton { } " +
-          "}",
-        ModesError.MULTIPLE_MODE_AUTOMATA),
-      // inner with two mode automata
-      arg("component Comp2 { " +
-          "component Inner { " +
-          "a.b.A a;" +
-          "mode automaton { } " +
-          "mode automaton { } " +
-          "} " +
-          "}",
-        ModesError.MULTIPLE_MODE_AUTOMATA)
+      // decomposed component with two mode automata
+      arg("component InvalidComp1 { a.b.A a; mode automaton { } mode automaton { } }",
+        MULTIPLE_MODE_AUTOMATA),
+      // nested decomposed component type with two mode automata
+      arg("component InvalidComp2 { component Inner { a.b.A a; mode automaton { } mode automaton { } } }",
+        MULTIPLE_MODE_AUTOMATA)
     );
   }
 }
