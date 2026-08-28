0fb2aa0787da46514439361a55cdbe29ed4e8f34
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 19:37:33 +0200

    Rework no init without compute tests

--- languages/montiarc/test/java/montiarc/_cocos/NoInitWithoutComputeTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/NoInitWithoutComputeTest.java
@@ -2,81 +2,129 @@
 package montiarc._cocos;
 
 import arccompute._cocos.NoInitWithoutCompute;
+import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
-import montiarc.ATestBase;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcComputeError;
 import montiarc.util.Error;
-
-import org.assertj.core.api.Assertions;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcComputeError.INIT_BLOCK_WITHOUT_COMPUTE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Tests for {@link NoInitWithoutCompute} CoCo:
- * Forbids 'init' blocks in components that also declare an automaton or that doesn't contain a compute block.
+ * The class under test is {@link NoInitWithoutCompute}.
  */
-public class NoInitWithoutComputeTest extends MontiArcTestBase {
+class NoInitWithoutComputeTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @MethodSource("validModels")
-  public void shouldNotReportError(@NotNull String model) throws IOException {
-    // parse & compile model into AST
-    ASTMACompilationUnit ast = MontiArcTestBase.compile(model);
+  void shouldNotReportError(@NotNull String model) {
+    Preconditions.checkNotNull(model);
 
-    // register CoCo and run checker
+    // Given
+    ASTMACompilationUnit ast = compile(model);
+
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new NoInitWithoutCompute());
+
+    // When
     checker.checkAll(ast);
 
-    // expect no findings
+    // Then
     assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error[] expectedErrors) throws IOException {
-    ASTMACompilationUnit ast = MontiArcTestBase.compile(model);
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
+    Preconditions.checkNotNull(model);
+    Preconditions.checkNotNull(errors);
+
+    // Given
+    ASTMACompilationUnit ast = compile(model);
 
     MontiArcCoCoChecker checker = new MontiArcCoCoChecker();
     checker.addCoCo(new NoInitWithoutCompute());
+
+    // When
     checker.checkAll(ast);
 
-    // expect findings and exact error codes (order-insensitive)
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
-    Assertions.assertThat(ATestBase.getLoggedErrorCodes()).containsExactlyInAnyOrder(ATestBase.getErrorCodes(expectedErrors));
+    // Then
+    assertThat(getLoggedErrorCodes())
+      .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> validModels() {
     return Stream.of(
-      Arguments.of("component comp1 {  init { int x = 1; } compute { } }"),
-      Arguments.of("component comp2 { automaton { initial state Init; } }"),
-      Arguments.of("component comp3 { compute { } }"),
-      Arguments.of("component comp4 { }")
+      // init block alongside a compute block
+      arg("""
+        component ValidComp1 {
+          init { int x = 1; }
+          compute { }
+        }
+        """
+      ),
+      // automaton and no init block
+      arg("""
+        component ValidComp2 {
+          automaton {
+            initial state Init;
+          }
+        }
+        """
+      ),
+      // compute block and no init block
+      arg("""
+        component ValidComp3 {
+          compute { }
+        }
+        """
+      ),
+      // component without any elements
+      arg("component ValidComp4 { }")
     );
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      Arguments.of(
-        "component comp5 { init { int x = 0; } automaton { initial state Init; } }",
-        new Error[]{ ArcComputeError.INIT_BLOCK_WITHOUT_COMPUTE }
+      // init block without a compute block, alongside an automaton
+      arg("""
+        component InvalidComp1 {
+          init { int x = 0; }
+          automaton {
+            initial state Init;
+          }
+        }
+        """,
+        INIT_BLOCK_WITHOUT_COMPUTE
       ),
-      Arguments.of(
-        "component comp6 { component comp7 { init { int y = 2; } automaton { initial state S; } } }",
-        new Error[]{ ArcComputeError.INIT_BLOCK_WITHOUT_COMPUTE }
+      // nested component type with an init block without a compute block
+      arg("""
+        component InvalidComp2 {
+          component Inner {
+            init { int y = 2; }
+            automaton {
+              initial state S;
+            }
+          }
+        }
+        """,
+        INIT_BLOCK_WITHOUT_COMPUTE
       ),
-      Arguments.of(
-        "component comp8 { init { int y = 2; } }",
-        new Error[]{ ArcComputeError.INIT_BLOCK_WITHOUT_COMPUTE }
+      // init block without a compute block, and no other elements
+      arg("""
+        component InvalidComp3 {
+          init { int y = 2; }
+        }
+        """,
+        INIT_BLOCK_WITHOUT_COMPUTE
       )
     );
   }
