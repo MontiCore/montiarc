035e45ac5d38788c1b9fb01fa53fd11671bd9032
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 21:42:17 +0200

    Rework max one initial state tests

--- languages/montiarc/test/java/montiarc/_cocos/MaxOneInitialStateTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/MaxOneInitialStateTest.java
@@ -10,84 +10,44 @@ import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc._visitor.MontiArcTraverser;
 import montiarc.util.Error;
-import montiarc.util.SCError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.SCError.MORE_THAN_ONE_INITIAL_STATE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The class under test is {@link MaxOneInitialState}.
  */
-public class MaxOneInitialStateTest extends MontiArcTestBase {
+class MaxOneInitialStateTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
     // no automaton
-    "component Comp1 { }",
-    // automaton no states
-    "component Comp2 {" +
-      "automaton { } " +
-      "}",
-    // automaton no initial state (multiple states)
-    "component Comp3 { " +
-      "automaton { " +
-      "state s1; " +
-      "state s2; " +
-      "} " +
-      "}",
-    // automaton with initial state
-    "component Comp4 { " +
-      "automaton { " +
-      "initial state s; " +
-      "} " +
-      "}",
-    // automaton with initial state (multiple states)
-    "component Comp5 { " +
-      "automaton { " +
-      "initial state s1; " +
-      "state s2; " +
-      "state s3; " +
-      "} " +
-      "}",
-    // inner automaton with initial state
-    "component Comp6 { " +
-      "component Inner { " +
-      "automaton { " +
-      "initial state s; " +
-      "} " +
-      "} " +
-      "}",
-    // two inner automata with initial state
-    "component Comp7 { " +
-      "component Inner1 { " +
-      "automaton { " +
-      "initial state s1; " +
-      "} " +
-      "} " +
-      "component Inner2 { " +
-      "automaton { " +
-      "initial state s2; " +
-      "} " +
-      "} " +
-      "}",
-    // mode automaton no states
-    "component Comp8 {" +
-      "mode automaton { } " +
-      "}",
-    // mode automaton and automaton no states
-    "component Comp9 {" +
-      "mode automaton { } " +
-      "automaton { } " +
-      "}",
+    "component ValidComp1 { }",
+    // automaton without states
+    "component ValidComp2 { automaton { } }",
+    // automaton without an initial state (multiple states)
+    "component ValidComp3 { automaton { state s1; state s2; } }",
+    // automaton with an initial state
+    "component ValidComp4 { automaton { initial state s; } }",
+    // automaton with an initial state (multiple states)
+    "component ValidComp5 { automaton { initial state s1; state s2; state s3; } }",
+    // nested component type with an automaton with an initial state
+    "component ValidComp6 { component Inner { automaton { initial state s; } } }",
+    // two nested component types, each with an automaton with an initial state
+    "component ValidComp7 { component Inner1 { automaton { initial state s1; } } component Inner2 { automaton { initial state s2; } } }",
+    // mode automaton without modes
+    "component ValidComp8 { mode automaton { } }",
+    // mode automaton and automaton, both without states
+    "component ValidComp9 { mode automaton { } automaton { } }",
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -102,12 +62,13 @@ public class MaxOneInitialStateTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -123,7 +84,6 @@ public class MaxOneInitialStateTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
@@ -131,41 +91,17 @@ public class MaxOneInitialStateTest extends MontiArcTestBase {
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
       // automaton with two initial states
-      arg("component Comp1 { " +
-          "automaton { " +
-          "initial state s1; " +
-          "initial state s2; " +
-          "} " +
-          "}",
-        SCError.MORE_THAN_ONE_INITIAL_STATE),
-      // inner automaton with two initial states
-      arg("component Comp2 { " +
-          "component Inner { " +
-          "automaton { " +
-          "initial state s1; " +
-          "initial state s2; " +
-          "} " +
-          "} " +
-        "}",
-        SCError.MORE_THAN_ONE_INITIAL_STATE),
-      // mode automaton with two initial state
-      arg("component Comp3 { " +
-          "mode automaton { " +
-          "initial mode s1 { } " +
-          "initial mode s2 { } " +
-          "} " +
-          "}",
-        SCError.MORE_THAN_ONE_INITIAL_STATE),
-      // mode automaton with two initial state and automaton
-      arg("component Comp4 { " +
-          "mode automaton { " +
-          "initial mode s1 { } " +
-          "initial mode s2 { } " +
-          "} " +
-          "automaton { " +
-          "} " +
-          "}",
-        SCError.MORE_THAN_ONE_INITIAL_STATE)
+      arg("component InvalidComp1 { automaton { initial state s1; initial state s2; } }",
+        MORE_THAN_ONE_INITIAL_STATE),
+      // nested component type with an automaton with two initial states
+      arg("component InvalidComp2 { component Inner { automaton { initial state s1; initial state s2; } } }",
+        MORE_THAN_ONE_INITIAL_STATE),
+      // mode automaton with two initial modes
+      arg("component InvalidComp3 { mode automaton { initial mode s1 { } initial mode s2 { } } }",
+        MORE_THAN_ONE_INITIAL_STATE),
+      // mode automaton with two initial modes, alongside a regular automaton
+      arg("component InvalidComp4 { mode automaton { initial mode s1 { } initial mode s2 { } } automaton { } }",
+        MORE_THAN_ONE_INITIAL_STATE)
     );
   }
 }
