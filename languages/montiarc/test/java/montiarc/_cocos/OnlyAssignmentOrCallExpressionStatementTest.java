30aba76d3c964c6723d377b0f72f0bef767e1eca
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 20:48:47 +0200

    Rework only assignment or call expression statement tests

--- languages/montiarc/test/java/montiarc/_cocos/OnlyAssignmentOrCallExpressionStatementTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/OnlyAssignmentOrCallExpressionStatementTest.java
@@ -4,27 +4,27 @@ package montiarc._cocos;
 import arcbasis._cocos.OnlyAssignmentOrCallExpressionStatement;
 import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
-import montiarc.ATestBase;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcError;
 import montiarc.util.Error;
-import org.assertj.core.api.Assertions;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.INVALID_STATEMENT;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class OnlyAssignmentOrCallExpressionStatementTest extends MontiArcTestBase {
+/**
+ * The class under test is {@link OnlyAssignmentOrCallExpressionStatement}.
+ */
+class OnlyAssignmentOrCallExpressionStatementTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @MethodSource("validModels")
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -41,27 +41,23 @@ public class OnlyAssignmentOrCallExpressionStatementTest extends MontiArcTestBas
 
   static Stream<Arguments> validModels() {
     return Stream.of(
-      Arguments.of(
-        "component Comp1 { }"
-      ),
-      Arguments.of(
-        "component Comp2 { int x = 0; compute { x++; } }"
-      ),
-      Arguments.of(
-        "component Comp3 { int x = 0; compute { x = 1 + 2; } }"
-      ),
-      Arguments.of(
-        "component Comp4 { int x = 0; compute { } init { x = 42; } }"
-      ),
-      Arguments.of(
-        "component Comp5 { compute { System.out.println(\"Test\"); } }"
-      )
+      // component without any statements
+      arg("component ValidComp1 { }"),
+      // increment expression statement
+      arg("component ValidComp2 { int x = 0; compute { x++; } }"),
+      // assignment expression statement
+      arg("component ValidComp3 { int x = 0; compute { x = 1 + 2; } }"),
+      // assignment expression statement in an init block
+      arg("component ValidComp4 { int x = 0; compute { } init { x = 42; } }"),
+      // call expression statement
+      arg("component ValidComp5 { compute { System.out.println(\"Test\"); } }")
     );
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -74,54 +70,67 @@ public class OnlyAssignmentOrCallExpressionStatementTest extends MontiArcTestBas
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
-    Assertions.assertThat(ATestBase.getLoggedErrorCodes()).containsExactlyInAnyOrder(ATestBase.getErrorCodes(errors));
+    assertThat(getLoggedErrorCodes())
+      .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   static Stream<Arguments> invalidModels() {
     return Stream.of(
+      // literal expression statement
       arg(
-        "component Comp1 { compute { 1; } }",
-        ArcError.INVALID_STATEMENT
+        "component InvalidComp1 { compute { 1; } }",
+        INVALID_STATEMENT
       ),
+      // bare variable reference expression statement
       arg(
-        "component Comp2 { int x = 0; compute { x; } }",
-        ArcError.INVALID_STATEMENT
+        "component InvalidComp2 { int x = 0; compute { x; } }",
+        INVALID_STATEMENT
       ),
+      // arithmetic expression statement
       arg(
-        "component Comp3 { int x = 0; compute { x + 1; } }",
-        ArcError.INVALID_STATEMENT
+        "component InvalidComp3 { int x = 0; compute { x + 1; } }",
+        INVALID_STATEMENT
       ),
+      // boolean literal, logical, and relational expression statements
       arg(
-        "component Comp4 { int x = 0;" +
-          "compute { " +
-          "true;" +
-          "b && true;" +
-          "b || true;" +
-          "1 == 2;" +
-          "1 > 2;" +
-          "}" +
-          "}",
-        ArcError.INVALID_STATEMENT, ArcError.INVALID_STATEMENT, ArcError.INVALID_STATEMENT, ArcError.INVALID_STATEMENT, ArcError.INVALID_STATEMENT
+        """
+        component InvalidComp4 {
+          int x = 0;
+          compute {
+            true;
+            b && true;
+            b || true;
+            1 == 2;
+            1 > 2;
+          }
+        }
+        """,
+        INVALID_STATEMENT, INVALID_STATEMENT, INVALID_STATEMENT, INVALID_STATEMENT, INVALID_STATEMENT
       ),
+      // parenthesized assignment expression statement (not a direct assignment)
       arg(
-        "component Comp5 { " +
-          "compute {" +
-          "int a = 0;" +
-          "(a = 0);" +
-          "} " +
-          "}",
-        ArcError.INVALID_STATEMENT
+        """
+        component InvalidComp5 {
+          compute {
+            int a = 0;
+            (a = 0);
+          }
+        }
+        """,
+        INVALID_STATEMENT
       ),
+      // parenthesized assignment expression statements alongside an if-statement and empty blocks
       arg(
-        "component Comp6 { " +
-          "compute {" +
-          "int i = 1;" +
-          "if (false); (i = 0);{};" +
-          "(i = 0);{}; " +
-          "} " +
-          "}",
-        ArcError.INVALID_STATEMENT, ArcError.INVALID_STATEMENT
+        """
+        component InvalidComp6 {
+          compute {
+            int i = 1;
+            if (false); (i = 0);{};
+            (i = 0);{};
+          }
+        }
+        """,
+        INVALID_STATEMENT, INVALID_STATEMENT
       )
     );
   }
