4267df99497ca0a87b21ef6f8639e7cf41c1469e
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 21:26:37 +0200

    Rework type param name is no reserved keyword tests

--- languages/montiarc/test/java/montiarc/_cocos/TypeParamNameIsNoReservedKeywordTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/TypeParamNameIsNoReservedKeywordTest.java
@@ -6,7 +6,6 @@ import de.se_rwth.commons.logging.Log;
 import arcbasis._cocos.TypeParamNameIsNoReservedKeyword;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcError;
 import montiarc.util.Error;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,29 +13,37 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.RESTRICTED_IDENTIFIER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The class under test is {@link TypeParamNameIsNoReservedKeyword}.
  */
-public class TypeParamNameIsNoReservedKeywordTest extends MontiArcTestBase {
+class TypeParamNameIsNoReservedKeywordTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
-    "package key; component Comp1 { }",
-    "component Comp2 { component I { } I key; }",
-    "component Comp3(int key) {  }",
-    "component Comp4<T> { }",
-    "component Comp5 { int key = 1; }",
-    "component Comp6 { port in int key; }",
-    "component Comp7 { automaton { state key; } }",
-    "component Comp8 { component key { } }",
+    // reserved keyword used as a package name
+    "package key; component ValidComp1 { }",
+    // reserved keyword used as a subcomponent instance name
+    "component ValidComp2 { component I { } I key; }",
+    // reserved keyword used as a parameter name
+    "component ValidComp3(int key) {  }",
+    // regular generic type parameter, no reserved keyword involved
+    "component ValidComp4<T> { }",
+    // reserved keyword used as a field name
+    "component ValidComp5 { int key = 1; }",
+    // reserved keyword used as a port name
+    "component ValidComp6 { port in int key; }",
+    // reserved keyword used as an automaton state name
+    "component ValidComp7 { automaton { state key; } }",
+    // reserved keyword used as a nested component type name
+    "component ValidComp8 { component key { } }",
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -49,12 +56,13 @@ public class TypeParamNameIsNoReservedKeywordTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -68,22 +76,25 @@ public class TypeParamNameIsNoReservedKeywordTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(errors.length);
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      arg("component Comp1<key> { }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp2<word> { }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp3<T, key> { }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp4<key, key> { }",
-        ArcError.RESTRICTED_IDENTIFIER,
-        ArcError.RESTRICTED_IDENTIFIER)
+      // single generic type parameter named after the first reserved keyword
+      arg("component InvalidComp1<key> { }",
+        RESTRICTED_IDENTIFIER),
+      // single generic type parameter named after the second reserved keyword
+      arg("component InvalidComp2<word> { }",
+        RESTRICTED_IDENTIFIER),
+      // two generic type parameters, one regular and one named after the keyword
+      arg("component InvalidComp3<T, key> { }",
+        RESTRICTED_IDENTIFIER),
+      // two generic type parameters, both named after the same reserved keyword
+      arg("component InvalidComp4<key, key> { }",
+        RESTRICTED_IDENTIFIER,
+        RESTRICTED_IDENTIFIER)
     );
   }
 }
