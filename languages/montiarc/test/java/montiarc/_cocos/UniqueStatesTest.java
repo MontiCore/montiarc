448187589527e72874faf4e8f1896cafa8a2af24
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 20:39:30 +0200

    Rework unique states tests

--- languages/montiarc/test/java/montiarc/_cocos/UniqueStatesTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/UniqueStatesTest.java
@@ -7,58 +7,36 @@ import de.se_rwth.commons.logging.Log;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
 import montiarc.util.Error;
-import montiarc.util.SCError;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.stream.Stream;
 
+import static montiarc.util.SCError.DUPLICATE_STATE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * The class under test is {@link UniqueStates}
+ * The class under test is {@link UniqueStates}.
  */
-public class UniqueStatesTest extends MontiArcTestBase {
+class UniqueStatesTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
     // no automaton
-    "component Comp1 { }",
-    // no states
-    "component Comp2 { " +
-      "automaton { } " +
-      "}",
-    // one state
-    "component Comp3 { " +
-      "automaton { " +
-      "initial state s; " +
-      "} " +
-      "}",
-    // multiple unique states
-    "component Comp4 { " +
-      "automaton { " +
-      "initial state s1; " +
-      "state s2; " +
-      "state s3; " +
-      "} " +
-      "}",
-    // inner with same states
-    "component Comp5 { " +
-      "automaton { " +
-      "initial state s; " +
-      "} " +
-      "component Inner { " +
-      "automaton { " +
-      "initial state s2; " +
-      "} " +
-      "} " +
-      "}"
+    "component ValidComp1 { }",
+    // automaton without states
+    "component ValidComp2 { automaton { } }",
+    // automaton with one state
+    "component ValidComp3 { automaton { initial state s; } }",
+    // automaton with multiple uniquely named states
+    "component ValidComp4 { automaton { initial state s1; state s2; state s3; } }",
+    // nested component with a state of the same name as the outer automaton's state
+    "component ValidComp5 { automaton { initial state s; } component Inner { automaton { initial state s2; } } }"
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -71,12 +49,13 @@ public class UniqueStatesTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -90,40 +69,48 @@ public class UniqueStatesTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindings()).as(Log.getFindings().toString()).isNotEmpty();
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      // two states same name
-      arg("component Comp1 { " +
-          "automaton { " +
-          "state s; " +
-          "state s; " +
-          "} " +
-          "}",
-        SCError.DUPLICATE_STATE),
-      // three states same name
-      arg("component Comp2 { " +
-          "automaton { " +
-          "state s; " +
-          "state s; " +
-          "state s; " +
-          "} " +
-          "}",
-        SCError.DUPLICATE_STATE),
-      // hierarchical states same name
-      arg("component Comp3 { " +
-          "automaton { " +
-          "state s1; " +
-          "state s2 { " +
-          "state s1; " +
-          "} " +
-          "} " +
-          "}",
-        SCError.DUPLICATE_STATE)
+      // two states with the same name
+      arg("""
+        component InvalidComp1 {
+          automaton {
+            state s;
+            state s;
+          }
+        }
+        """,
+        DUPLICATE_STATE
+      ),
+      // three states with the same name
+      arg("""
+        component InvalidComp2 {
+          automaton {
+            state s;
+            state s;
+            state s;
+          }
+        }
+        """,
+        DUPLICATE_STATE
+      ),
+      // nested state with the same name as an outer state
+      arg("""
+        component InvalidComp3 {
+          automaton {
+            state s1;
+            state s2 {
+              state s1;
+            }
+          }
+        }
+        """,
+        DUPLICATE_STATE
+      )
     );
   }
 }
