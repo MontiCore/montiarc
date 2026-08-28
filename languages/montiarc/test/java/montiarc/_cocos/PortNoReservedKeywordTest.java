19ff0497c1d9f1460f3f0feb941c16d54c9fbc6f
David Schmalzing <schmalzing@se-rwth.de>
2026-08-28 20:31:55 +0200

    Rework port no reserved keyword tests

--- languages/montiarc/test/java/montiarc/_cocos/PortNoReservedKeywordTest.java
+++ languages/montiarc/test/java/montiarc/_cocos/PortNoReservedKeywordTest.java
@@ -6,7 +6,6 @@ import com.google.common.base.Preconditions;
 import de.se_rwth.commons.logging.Log;
 import montiarc.MontiArcTestBase;
 import montiarc._ast.ASTMACompilationUnit;
-import montiarc.util.ArcError;
 import montiarc.util.Error;
 import org.codehaus.commons.nullanalysis.NotNull;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,29 +13,37 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
+import static montiarc.util.ArcError.RESTRICTED_IDENTIFIER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * The class under test is {@link PortNoReservedKeyword}.
  */
-public class PortNoReservedKeywordTest extends MontiArcTestBase {
+class PortNoReservedKeywordTest extends MontiArcTestBase {
 
   @ParameterizedTest
   @ValueSource(strings = {
-    "package key; component Comp1 { }",
-    "component Comp2 { component I { } I key; }",
-    "component Comp3(int key) {  }",
-    "component Comp4<key> { }",
-    "component Comp5 { int key = 1; }",
-    "component Comp6 { port in int i; }",
-    "component Comp7 { automaton { state key; } }",
-    "component Comp8 { component key { } }",
+    // reserved keyword used as a package name
+    "package key; component ValidComp1 { }",
+    // reserved keyword used as a subcomponent instance name
+    "component ValidComp2 { component I { } I key; }",
+    // reserved keyword used as a parameter name
+    "component ValidComp3(int key) {  }",
+    // reserved keyword used as a generic type parameter name
+    "component ValidComp4<key> { }",
+    // reserved keyword used as a field name
+    "component ValidComp5 { int key = 1; }",
+    // regular port, no reserved keyword involved
+    "component ValidComp6 { port in int i; }",
+    // reserved keyword used as an automaton state name
+    "component ValidComp7 { automaton { state key; } }",
+    // reserved keyword used as a nested component type name
+    "component ValidComp8 { component key { } }",
   })
-  public void shouldNotReportError(@NotNull String model) throws IOException {
+  void shouldNotReportError(@NotNull String model) {
     Preconditions.checkNotNull(model);
 
     // Given
@@ -49,12 +56,13 @@ public class PortNoReservedKeywordTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(0);
+    assertThat(Log.getFindings()).isEmpty();
   }
 
   @ParameterizedTest
   @MethodSource("invalidModels")
-  public void shouldReportError(@NotNull String model, @NotNull Error... errors) throws IOException {
+  void shouldReportError(@NotNull String model,
+                         @NotNull Error... errors) {
     Preconditions.checkNotNull(model);
     Preconditions.checkNotNull(errors);
 
@@ -68,32 +76,39 @@ public class PortNoReservedKeywordTest extends MontiArcTestBase {
     checker.checkAll(ast);
 
     // Then
-    assertThat(Log.getFindingsCount()).as(Log.getFindings().toString()).isEqualTo(errors.length);
     assertThat(getLoggedErrorCodes())
       .containsExactlyInAnyOrder(getErrorCodes(errors));
   }
 
   protected static Stream<Arguments> invalidModels() {
     return Stream.of(
-      arg("component Comp1 { port in int key; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp2 { port in int word; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp3 { port in int a, key; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp4 { port in int key, key; }",
-        ArcError.RESTRICTED_IDENTIFIER,
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp5 { port in int a, out int key; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp6 { port in int key, out int key; }",
-        ArcError.RESTRICTED_IDENTIFIER,
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp7 { port in int a; port out int key; }",
-        ArcError.RESTRICTED_IDENTIFIER),
-      arg("component Comp8 { port in int key; port out int key; }",
-        ArcError.RESTRICTED_IDENTIFIER,
-        ArcError.RESTRICTED_IDENTIFIER)
+      // single port named after the first reserved keyword
+      arg("component InvalidComp1 { port in int key; }",
+        RESTRICTED_IDENTIFIER),
+      // single port named after the second reserved keyword
+      arg("component InvalidComp2 { port in int word; }",
+        RESTRICTED_IDENTIFIER),
+      // two ports declared together, one regular and one named after the keyword
+      arg("component InvalidComp3 { port in int a, key; }",
+        RESTRICTED_IDENTIFIER),
+      // two ports declared together, both named after the same keyword
+      arg("component InvalidComp4 { port in int key, key; }",
+        RESTRICTED_IDENTIFIER,
+        RESTRICTED_IDENTIFIER),
+      // two ports with different directions declared together, one regular and one named after the keyword
+      arg("component InvalidComp5 { port in int a, out int key; }",
+        RESTRICTED_IDENTIFIER),
+      // two ports with different directions declared together, both named after the same keyword
+      arg("component InvalidComp6 { port in int key, out int key; }",
+        RESTRICTED_IDENTIFIER,
+        RESTRICTED_IDENTIFIER),
+      // two ports declared separately, one regular and one named after the keyword
+      arg("component InvalidComp7 { port in int a; port out int key; }",
+        RESTRICTED_IDENTIFIER),
+      // two ports declared separately, both named after the same keyword
+      arg("component InvalidComp8 { port in int key; port out int key; }",
+        RESTRICTED_IDENTIFIER,
+        RESTRICTED_IDENTIFIER)
     );
   }
 }
